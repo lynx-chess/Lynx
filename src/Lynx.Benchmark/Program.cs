c28754502e4d3280e47d2e74014fb175e4dba536
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2021-05-03 01:04:26 +0200

    Rename Benchmarks action

--- src/Lynx.Benchmark/Program.cs
+++ src/Lynx.Benchmark/Program.cs
@@ -2,7 +2,7 @@
 using BenchmarkDotNet.Running;
 using System.Reflection;
 
-//SharpFish.Benchmark.BitBoard_Struct_ReadonlyStruct_Class_Record.SizeTest();
+//Lynx.Benchmark.BitBoard_Struct_ReadonlyStruct_Class_Record.SizeTest();
 
 #if DEBUG
 BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args, new DebugInProcessConfig());
