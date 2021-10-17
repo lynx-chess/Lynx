fc7313ae936c31e1cd5fb76daf696b2d7d4c130d
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2021-10-17 02:47:41 +0200

    Increase `DepthWhenLessThanMinMoveTime` to 3. The original intention was to increase `MinDepth` as well to 5, but rq2k2r/ppp2pb1/2n1pnpp/1Q1p1b2/3P1B2/2N1PNP1/PPP2PBP/R3K2R w KQkq - 0 1. If we increase `DepthWhenLessThanMinMoveTime` we end up witht he same min over and under a minute, so keeping it at 3 for now.

--- src/Lynx/Constants.cs
+++ src/Lynx/Constants.cs
@@ -298,6 +298,8 @@ namespace Lynx
         public const string KillerTestPositionFEN = "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1";
         public const string CmkTestPositionFEN = "r2q1rk1/ppp2ppp/2n1bn2/2b1p3/3pP3/3P1NPP/PPP1NPB1/R1BQ1RK1 b - - 0 9";
 
+        public const string ComplexPositionFEN = "rq2k2r/ppp2pb1/2n1pnpp/1Q1p1b2/3P1B2/2N1PNP1/PPP2PBP/R3K2R w KQkq - 0 1";
+
         public const string EmptyBoardFEN = "8/8/8/8/8/8/8/8 w - - 0 1";
 
         public const int WhiteShortCastleKingSquare = (int)BoardSquare.g1;
