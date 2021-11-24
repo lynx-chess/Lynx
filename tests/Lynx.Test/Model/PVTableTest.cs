c6fa7b04781be250b8a6902c86e58c3322c119ba
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2021-11-24 01:10:16 +0100

    🐛 Fix crash when depth reaches MaxDepth Ensure `PVTable.Indexes` support `Configuration.EngineSettings.MaxDepth`, since NegaMax populates `pvTable[currentDepth + 1]`

---
+++ tests/Lynx.Test/Model/PVTableTest.cs
@@ -0,0 +1,13 @@
+﻿using Lynx.Model;
+using NUnit.Framework;
+
+namespace Lynx.Test.Model;
+
+public class PVTableTest
+{
+    [Test]
+    public void PVTable_Indexes_Support_MaxDepthPlusOne()
+    {
+        Assert.DoesNotThrow(() => _ = PVTable.Indexes[Configuration.EngineSettings.MaxDepth]);
+    }
+}
