f90deb4b22c6a81c189a5c3ea0cf175ca9eaed1a
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2021-12-07 22:31:24 +0100

    WIP

---
+++ src/Lynx/Model/TranspositionTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lynx.Model;
+
+public enum ElementType
+{
+    Exact,
+    Alpha,
+    Beta
+}
+public struct TranspositionTableElement
+{
+    public long Key { get; set; }
+
+    public ElementType Type { get; set; }
+
+    public int Score { get; set; }
+
+    public Move Move { get; set; }
+
+    public int Depth { get; set; }
+}
+
+public class TranspositionTable : HashSet<TranspositionTableElement>
+{
+    public TranspositionTable(int capacity) : base(capacity)
+    {
+    }
+}
