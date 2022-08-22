435bdb83e282f34cbb7624c2839347c7dd029314
Eduardo Caceres <eduardo.caceres@microsoft.com>
2022-08-22 17:29:48 +0200

    Add sample test

---
+++ tests/Lynx.Test/BestMove/Categories.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+public class SampleClass
+{
+
+    [Test]
+    [Explicit]
+    [Category("LongRunning")]
+    public void __________________________SampleTest(){ Assert.True(true); }
+}
