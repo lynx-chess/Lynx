c9e5e3809e7cd487b70ac71921565e756fd44277
Eduardo Caceres <eduardo.caceres@microsoft.com>
2022-08-22 17:33:14 +0200

    Add change to test it out

--- tests/Lynx.Test/Categories.cs
+++ tests/Lynx.Test/Categories.cs
@@ -5,6 +5,7 @@ public static class Categories
     public const string Perft = "Perft";
 
     public const string LongRunning = "LongRunning";
+    // public const string LongRunning = "LongRunningg"; Change with this
 
     public const string TooLong = "TooLongToBeRun";
 
