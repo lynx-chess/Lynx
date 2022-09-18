a5f3c8b57288f2fa48f63603c3d52f2156e54743
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2022-09-18 16:38:10 +0200

    Prevent NUnit from doing non-explicit test runs
    Prevent NUnit from doing non-explicit test runs, which was in practice skipping a bunch of tests from being run in CI.

--- tests/Lynx.Test/BestMove/SacrificesTest.cs
+++ tests/Lynx.Test/BestMove/SacrificesTest.cs
@@ -4,8 +4,10 @@ namespace Lynx.Test.BestMove;
 
 public class SacrificesTest : BaseTest
 {
+    [Explicit]
+    [Category(Categories.LongRunning)]
     [TestCase("4n3/1p2k2p/p2p2pP/P1bP1pP1/2P2P2/2BB4/3K4/8 w - - 0 43", new[] { "d3f5" },
-        Category = Categories.LongRunning, Explicit = true, Description = "Actual Bishop sacrifice - https://lichess.org/VaY6zfHI/white#84")]
+        Description = "Actual Bishop sacrifice - https://lichess.org/VaY6zfHI/white#84")]
     public void Sacrifices(string fen, string[]? allowedUCIMoveString, string[]? excludedUCIMoveString = null)
     {
         TestBestMove(fen, allowedUCIMoveString, excludedUCIMoveString, depth: 12);
