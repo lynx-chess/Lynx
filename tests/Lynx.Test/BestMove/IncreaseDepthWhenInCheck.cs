6657773ec6b5e43d643c5aced8b22bc74d229d91
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-07-20 15:14:37 +0200

    Add test

---
+++ tests/Lynx.Test/BestMove/IncreaseDepthWhenInCheck.cs
@@ -0,0 +1,33 @@
+﻿using Lynx.Model;
+using Lynx.UCI.Commands.GUI;
+using NUnit.Framework;
+
+namespace Lynx.Test.BestMove;
+public class IncreaseDepthWhenInCheck : BaseTest
+{
+    /// <summary>
+    /// Position:
+    /// 8   . r . . . . . .
+    /// 7   . . . . . . . .
+    /// 6   . . . . . . . .
+    /// 5   . . . . . . k P
+    /// 4   K . . . . . . .
+    /// 3   . . . . . R . .
+    /// 2   . q . . . . . .
+    /// 1   R . . . . . q .
+    ///
+    ///     a b c d e f g h
+    /// </summary>
+    [Test]
+    public void DepthLimit()
+    {
+        Configuration.EngineSettings.MinDepth = 0;
+
+        var engine = GetEngine("1r6/8/8/6kP/K7/5R1p/1q6/R5q1 w - - 0 2");
+        Assert.AreEqual(Side.White, engine.Game.CurrentPosition.Side);
+
+        var searchResult = engine.BestMove(new GoCommand("go depth 1"));
+
+        Assert.AreEqual("g5h4", searchResult.Moves[1].UCIString());
+    }
+}
