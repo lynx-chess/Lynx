3279cff9630f22054b418e07c7d9ae73f8078690
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-08-03 19:53:29 +0200

    Use `Array.LongLength`

--- src/Lynx/Model/TranspositionTable.cs
+++ src/Lynx/Model/TranspositionTable.cs
@@ -67,7 +67,7 @@ public static class TranspositionTableExtensions
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static int TranspositionTableIndex(Position position, TranspositionTable transpositionTable) =>
-        (int)((position.UniqueIdentifier * transpositionTable.Length) >> 32);
+        (int)((position.UniqueIdentifier * transpositionTable.LongLength) >> 32);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void ClearTranspositionTable(this TranspositionTable transpositionTable)
