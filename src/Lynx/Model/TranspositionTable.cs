8c4793e5cb8178151112d60d394047ad26cec914
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-08-10 01:28:27 +0200

    Avoid using cutoffs in QSearch

--- src/Lynx/Model/TranspositionTable.cs
+++ src/Lynx/Model/TranspositionTable.cs
@@ -105,7 +105,7 @@ public static class TranspositionTableExtensions
 
         var eval = EvaluationConstants.NoHashEntry;
 
-        if (entry.Depth >= (targetDepth - ply))
+        if (entry.Depth >= (targetDepth - ply) && entry.Depth > 0)
         {
             // We want to translate the checkmate position relative to the saved node to our root position from which we're searching
             // If the recorded score is a checkmate in 3 and we are at depth 5, we want to read checkmate in 8
