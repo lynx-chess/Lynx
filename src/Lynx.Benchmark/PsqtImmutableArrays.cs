fc9326550ececa90fbb4e4edc5dd694e73440802
Eduardo Caceres <edcaceres@microsoft.com>
2023-09-06 15:42:39 +0200

    Add 100_000 benchmark

--- src/Lynx.Benchmark/PsqtImmutableArrays.cs
+++ src/Lynx.Benchmark/PsqtImmutableArrays.cs
@@ -6,7 +6,7 @@ using System.Collections.Immutable;
 namespace Lynx.Benchmark;
 public class PsqtImmutableArrays : BaseBenchmark
 {
-    public static IEnumerable<int> Data => new[] { 1, 10, 1_000, 10_000 };
+    public static IEnumerable<int> Data => new[] { 1, 10, 1_000, 10_000, 100_000, };
 
     public static IEnumerable<Position> Positions => new Position[] {
             new(Constants.InitialPositionFEN),
