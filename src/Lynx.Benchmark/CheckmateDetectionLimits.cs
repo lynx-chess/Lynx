464cf4a03936a089edd059dda78b35dc76744c59
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-09-08 17:10:22 +0200

    Add CheckmateDetectionLimits benchmark

---
+++ src/Lynx.Benchmark/CheckmateDetectionLimits.cs
@@ -0,0 +1,40 @@
+﻿/*
+ */
+
+using BenchmarkDotNet.Attributes;
+using Lynx.Model;
+
+namespace Lynx.Benchmark;
+
+public class CheckmateDetectionLimits : BaseBenchmark
+{
+    public static IEnumerable<int> Data => new[] { 1, 10, 1_000, 10_000, 100_000 };
+
+    [Benchmark(Baseline = true)]
+    [ArgumentsSource(nameof(Data))]
+    public bool LessAndGreater(int iterations)
+    {
+        bool result = false;
+
+        for (int i = 0; i < iterations; ++i)
+        {
+            result ^= (iterations < EvaluationConstants.PositiveCheckmateDetectionLimit && iterations > -EvaluationConstants.PositiveCheckmateDetectionLimit);
+        }
+
+        return result;
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Data))]
+    public bool Abs(int iterations)
+    {
+        bool result = false;
+
+        for (int i = 0; i < iterations; ++i)
+        {
+            result ^= (Math.Abs(iterations) < EvaluationConstants.PositiveCheckmateDetectionLimit);
+        }
+
+        return result;
+    }
+}
