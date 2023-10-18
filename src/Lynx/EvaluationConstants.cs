cc1cb2d26565872324a7c5dda2f14361814796fd
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2023-10-18 15:24:40 +0200

    Increase MaxHistoryMoveValue to 4_096

--- src/Lynx/EvaluationConstants.cs
+++ src/Lynx/EvaluationConstants.cs
@@ -316,7 +316,7 @@ public static class EvaluationConstants
 
     public const int PromotionMoveScoreValue = 131_072;
 
-    public const int MaxHistoryMoveValue = 2_048;
+    public const int MaxHistoryMoveValue = 4_096;
 
     /// <summary>
     /// Negative offset to ensure history move scores don't reach other move ordering values
