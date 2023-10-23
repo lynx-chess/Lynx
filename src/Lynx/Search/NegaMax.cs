8094dc7513e0205764886a1e35aa878dc29d0229
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-10-23 02:33:30 +0200

    Revert "Check tm every 2048 nodes"
    This reverts commit 4c97116f09236e4fda134bda916986befcac47b7.

--- src/Lynx/Search/NegaMax.cs
+++ src/Lynx/Search/NegaMax.cs
@@ -32,12 +32,7 @@ public sealed partial class Engine
         }
 
         _maxDepthReached[ply] = ply;
-        var checkTime = _nodes % Configuration.EngineSettings.CheckTime == 0;
-
-        if (checkTime)
-        {
-            _absoluteSearchCancellationTokenSource.Token.ThrowIfCancellationRequested();
-        }
+        _absoluteSearchCancellationTokenSource.Token.ThrowIfCancellationRequested();
 
         var pvIndex = PVTable.Indexes[ply];
         var nextPvIndex = PVTable.Indexes[ply + 1];
@@ -58,10 +53,7 @@ public sealed partial class Engine
         }
 
         // Before any time-consuming operations
-        if (checkTime)
-        {
-            _searchCancellationTokenSource.Token.ThrowIfCancellationRequested();
-        }
+        _searchCancellationTokenSource.Token.ThrowIfCancellationRequested();
 
         bool isInCheck = position.IsInCheck();
 
@@ -347,11 +339,8 @@ public sealed partial class Engine
     {
         var position = Game.CurrentPosition;
 
-        if (_nodes % 2048 == 0)
-        {
-            _absoluteSearchCancellationTokenSource.Token.ThrowIfCancellationRequested();
-            _searchCancellationTokenSource.Token.ThrowIfCancellationRequested();
-        }
+        _absoluteSearchCancellationTokenSource.Token.ThrowIfCancellationRequested();
+        _searchCancellationTokenSource.Token.ThrowIfCancellationRequested();
 
         if (ply >= Configuration.EngineSettings.MaxDepth)
         {
