26bfba4f573fc00d47944e5eb34a24de277d276e
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-10-24 11:12:53 +0200

    Cleanup

--- src/Lynx/Search/NegaMax.cs
+++ src/Lynx/Search/NegaMax.cs
@@ -221,8 +221,7 @@ public sealed partial class Engine
                 evaluation = -NegaMax(depth - 1 - reduction, ply + 1, -alpha - 1, -alpha, isVerifyingNullMoveCutOff);
 
                 // 🔍 Principal Variation Search (PVS)
-                if (evaluation > alpha
-                    && reduction > 0)            // LMR failed)
+                if (evaluation > alpha && reduction > 0)
                 {
                     // Optimistic search, validating that the rest of the moves are worse than bestmove.
                     // It should produce more cutoffs and therefore be faster.
