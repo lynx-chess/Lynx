527ed5d8960902754512c3d690aa632f5ad3e0b6
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-10-24 15:19:42 +0200

    Don't clear TT move if no best move is provided: keep old one

--- src/Lynx/Model/TranspositionTable.cs
+++ src/Lynx/Model/TranspositionTable.cs
@@ -172,7 +172,7 @@ public static class TranspositionTableExtensions
         entry.Key = position.UniqueIdentifier;
         entry.Score = score;
         entry.Depth = depth;
-        entry.Move = move ?? 0;
+        entry.Move = move ?? entry.Move;
         entry.Type = nodeType;
     }
 
