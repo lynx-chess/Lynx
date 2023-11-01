2b482e647a3adcc34b7ed72c930b64d33949f31e
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-11-01 03:28:32 +0100

    Add benchmark

---
+++ src/Lynx.Benchmark/And_Sum_Or.cs
@@ -0,0 +1,64 @@
+﻿/*
+ *
+ *
+ */
+
+using BenchmarkDotNet.Attributes;
+
+namespace Lynx.Benchmark;
+
+public class And_Sum_Or : BaseBenchmark
+{
+    public static IEnumerable<int> Data => new[] { 1, 10, 1_000, 10_000, 100_000 };
+
+    [Benchmark(Baseline = true)]
+    [ArgumentsSource(nameof(Data))]
+    public bool And(int data)
+    {
+        bool sum = false;
+        for (int i = 0; i < data; ++i)
+        {
+            int a = i % 2;
+            int b = i % 3;
+            int c = i % 6;
+
+            sum ^= (a == 0 && b == 0 && c == 0);
+        }
+
+        return sum;
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Data))]
+    public bool Sum(int data)
+    {
+        bool sum = false;
+        for (int i = 0; i < data; ++i)
+        {
+            int a = i % 2;
+            int b = i % 3;
+            int c = i % 6;
+
+            sum ^= (a + b + c == 0);
+        }
+
+        return sum;
+    }
+
+    [Benchmark]
+    [ArgumentsSource(nameof(Data))]
+    public bool Or(int data)
+    {
+        bool sum = false;
+        for (int i = 0; i < data; ++i)
+        {
+            int a = i % 2;
+            int b = i % 3;
+            int c = i % 6;
+
+            sum ^= ((a | b | c) == 0);
+        }
+
+        return sum;
+    }
+}
