c4b8ab9c63692f20c18a9d321164f8544e27c3c0
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-12-15 02:00:36 +0100

    Rename benchmark

--- src/Lynx.Benchmark/BenchUCI.cs
+++ src/Lynx.Benchmark/BenchUCI.cs
@@ -3,7 +3,7 @@ using System.Threading.Channels;
 
 namespace Lynx.Benchmark;
 
-public class Bench : BaseBenchmark
+public class BenchUCI : BaseBenchmark
 {
     private readonly Channel<string> _channel = Channel.CreateBounded<string>(new BoundedChannelOptions(100_000) { SingleReader = true, SingleWriter = false });
 
