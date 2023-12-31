760c4d5753aa5a9d8822970a356e7770161df8e8
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2023-12-31 22:07:47 +0100

    🔍 Use spsa tuned search values (#553)

--- src/Lynx/Configuration.cs
+++ src/Lynx/Configuration.cs
@@ -122,38 +122,38 @@ public sealed class EngineSettings
     /// <summary>
     /// Value originally from Stormphrax, who apparently took it from Viridithas
     /// </summary>
-    public double LMR_Base { get; set; } = 0.74;
+    public double LMR_Base { get; set; } = 0.85;
 
     /// <summary>
     /// Value originally from Akimbo
     /// </summary>
-    public double LMR_Divisor { get; set; } = 2.94;
+    public double LMR_Divisor { get; set; } = 2.84;
 
     public int NMP_MinDepth { get; set; } = 3;
 
     public int NMP_BaseDepthReduction { get; set; } = 1;
 
-    public int AspirationWindow_Delta { get; set; } = 16;
+    public int AspirationWindow_Delta { get; set; } = 20;
 
     public int AspirationWindow_MinDepth { get; set; } = 7;
 
     public int RFP_MaxDepth { get; set; } = 4;
 
-    public int RFP_DepthScalingFactor { get; set; } = 85;
+    public int RFP_DepthScalingFactor { get; set; } = 87;
 
     public int Razoring_MaxDepth { get; set; } = 3;
 
-    public int Razoring_Depth1Bonus { get; set; } = 106;
+    public int Razoring_Depth1Bonus { get; set; } = 105;
 
-    public int Razoring_NotDepth1Bonus { get; set; } = 156;
+    public int Razoring_NotDepth1Bonus { get; set; } = 161;
 
     public int IIR_MinDepth { get; set; } = 2;
 
-    public int LMP_MaxDepth { get; set; } = 1;
+    public int LMP_MaxDepth { get; set; } = 2;
 
-    public int LMP_BaseMovesToTry { get; set; } = 1;
+    public int LMP_BaseMovesToTry { get; set; } = 0;
 
-    public int LMP_MovesDepthMultiplier { get; set; } = 11;
+    public int LMP_MovesDepthMultiplier { get; set; } = 10;
 
     public int History_MaxMoveValue { get; set; } = 8_192;
 
