28a5a5c7e72edf9c813c03381a1ad2417cf5ff4f
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-01-02 21:13:38 +0100

    Update enpassant benchmarks

--- src/Lynx.Benchmark/ZobristHash_EnPassant.cs
+++ src/Lynx.Benchmark/ZobristHash_EnPassant.cs
@@ -8,8 +8,8 @@
  *
  *  | Method   | Mean      | Error     | StdDev    | Ratio | RatioSD | Allocated | Alloc Ratio |
  *  |--------- |----------:|----------:|----------:|------:|--------:|----------:|------------:|
- *  | Module   | 0.3194 ns | 0.0039 ns | 0.0032 ns |  1.00 |    0.00 |         - |          NA |
- *  | AndTrick | 0.3471 ns | 0.0159 ns | 0.0141 ns |  1.09 |    0.05 |         - |          NA |
+ *  | Module   | 0.3307 ns | 0.0075 ns | 0.0066 ns |  1.00 |    0.00 |         - |          NA |
+ *  | AndTrick | 0.3286 ns | 0.0071 ns | 0.0056 ns |  0.99 |    0.03 |         - |          NA |
  *
  *
  *  BenchmarkDotNet v0.13.11, Windows 10 (10.0.20348.2159) (Hyper-V)
@@ -17,23 +17,22 @@
  *  .NET SDK 8.0.100
  *    [Host]     : .NET 8.0.0 (8.0.23.53103), X64 RyuJIT AVX2
  *    DefaultJob : .NET 8.0.0 (8.0.23.53103), X64 RyuJIT AVX2
- *
  *  | Method   | Mean      | Error     | StdDev    | Ratio | Allocated | Alloc Ratio |
  *  |--------- |----------:|----------:|----------:|------:|----------:|------------:|
- *  | Module   | 0.5044 ns | 0.0036 ns | 0.0030 ns |  1.00 |         - |          NA |
- *  | AndTrick | 0.5051 ns | 0.0034 ns | 0.0027 ns |  1.00 |         - |          NA |
+ *  | Module   | 0.5209 ns | 0.0014 ns | 0.0013 ns |  1.00 |         - |          NA |
+ *  | AndTrick | 0.4598 ns | 0.0018 ns | 0.0014 ns |  0.88 |         - |          NA |
  *
  *
  *  BenchmarkDotNet v0.13.11, macOS Monterey 12.7.2 (21G1974) [Darwin 21.6.0]
- *  Intel Core i7-8700B CPU 3.20GHz (Max: 3.19GHz) (Coffee Lake), 1 CPU, 4 logical and 4 physical cores
+ *  Intel Xeon CPU E5-1650 v2 3.50GHz (Max: 3.34GHz), 1 CPU, 3 logical and 3 physical cores
  *  .NET SDK 8.0.100
- *    [Host]     : .NET 8.0.0 (8.0.23.53103), X64 RyuJIT AVX2
- *    DefaultJob : .NET 8.0.0 (8.0.23.53103), X64 RyuJIT AVX2
+ *    [Host]     : .NET 8.0.0 (8.0.23.53103), X64 RyuJIT AVX
+ *    DefaultJob : .NET 8.0.0 (8.0.23.53103), X64 RyuJIT AVX
  *
  *  | Method   | Mean      | Error     | StdDev    | Median    | Ratio | RatioSD | Allocated | Alloc Ratio |
  *  |--------- |----------:|----------:|----------:|----------:|------:|--------:|----------:|------------:|
- *  | Module   | 0.4056 ns | 0.0725 ns | 0.2033 ns | 0.3153 ns |  1.00 |    0.00 |         - |          NA |
- *  | AndTrick | 0.1020 ns | 0.0627 ns | 0.1849 ns | 0.0000 ns |  0.34 |    0.66 |         - |          NA |
+ *  | Module   | 0.5491 ns | 0.0484 ns | 0.1092 ns | 0.5070 ns |  1.00 |    0.00 |         - |          NA |
+ *  | AndTrick | 0.7057 ns | 0.0292 ns | 0.0287 ns | 0.6935 ns |  1.18 |    0.27 |         - |          NA |
  *
  */
 
