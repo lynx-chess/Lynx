89195330e8df472d608783f6fd246ecacdafc445
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-01-03 05:05:31 +0100

    Fix test

--- tests/Lynx.Test/ZobristTableTest.cs
+++ tests/Lynx.Test/ZobristTableTest.cs
@@ -51,15 +51,15 @@ public class ZobristTableTest
     [Test]
     public void EnPassantHash()
     {
-        var squares = Constants.EnPassantCaptureSquares.Where(content => content != 0).Select((_, index) => index);
+        var enPassantSquares = Constants.EnPassantCaptureSquares.Select((item, index) => (item, index)).Where(pair => pair.item != 0).Select(pair => pair.index);
 
-        foreach (var enPassantSquare in squares)
+        foreach (var enPassantSquare in enPassantSquares)
         {
             var file = enPassantSquare % 8;
             Assert.AreEqual(_zobristTable[file, (int)Piece.P], ZobristTable.EnPassantHash(enPassantSquare));
         }
 
-        Assert.AreEqual(16, squares.Count());
+        Assert.AreEqual(16, enPassantSquares.Count());
 
 #if DEBUG
         for (int square = 0; square < 64; ++square)
