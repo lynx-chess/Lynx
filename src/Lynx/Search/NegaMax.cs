f67823e7a949d9729cdc92fa60d26217510108b9
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-01-20 18:26:03 +0100

    Simplify triple repetition detection logic, removing some branching

--- src/Lynx/Search/NegaMax.cs
+++ src/Lynx/Search/NegaMax.cs
@@ -221,7 +221,7 @@ public sealed partial class Engine
             var isThreeFoldRepetition = !Game.PositionHashHistory.Add(position.UniqueIdentifier);
 
             int evaluation;
-            if (isThreeFoldRepetition || Game.Is50MovesRepetition())
+            if (isThreeFoldRepetition)
             {
                 evaluation = 0;
 
@@ -229,10 +229,24 @@ public sealed partial class Engine
                 // Since we won't be evaluating further down, we need to clear the PV table because those moves there
                 // don't belong to this line and if this move were to beat alpha, they'd incorrectly copied to pv line.
                 Array.Clear(_pVTable, nextPvIndex, _pVTable.Length - nextPvIndex);
+
+                // This is the only case were we don't clear position.UniqueIdentifier from Game.PositionHashHistory, because it was already there before making the move
+            }
+            else if (Game.Is50MovesRepetition())
+            {
+                evaluation = 0;
+
+                // We don't need to evaluate further down to know it's a draw.
+                // Since we won't be evaluating further down, we need to clear the PV table because those moves there
+                // don't belong to this line and if this move were to beat alpha, they'd incorrectly copied to pv line.
+                Array.Clear(_pVTable, nextPvIndex, _pVTable.Length - nextPvIndex);
+
+                Game.PositionHashHistory.Remove(position.UniqueIdentifier);
             }
             else if (pvNode && movesSearched == 0)
             {
                 evaluation = -NegaMax(depth - 1, ply + 1, -beta, -alpha);
+                Game.PositionHashHistory.Remove(position.UniqueIdentifier);
             }
             else
             {
@@ -246,10 +260,7 @@ public sealed partial class Engine
                 {
                     // After making a move
                     Game.HalfMovesWithoutCaptureOrPawnMove = oldValue;
-                    if (!isThreeFoldRepetition)
-                    {
-                        Game.PositionHashHistory.Remove(position.UniqueIdentifier);
-                    }
+                    Game.PositionHashHistory.Remove(position.UniqueIdentifier); // We know that there's no triple repetition here
                     position.UnmakeMove(move, gameState);
 
                     break;
@@ -311,14 +322,13 @@ public sealed partial class Engine
                     // PVS Hipothesis invalidated -> search with full depth and full score bandwidth
                     evaluation = -NegaMax(depth - 1, ply + 1, -beta, -alpha);
                 }
+
+                Game.PositionHashHistory.Remove(position.UniqueIdentifier);
             }
 
             // After making a move
+            // Game.PositionHashHistory is update above
             Game.HalfMovesWithoutCaptureOrPawnMove = oldValue;
-            if (!isThreeFoldRepetition)
-            {
-                Game.PositionHashHistory.Remove(position.UniqueIdentifier);
-            }
             position.UnmakeMove(move, gameState);
 
             PrintMove(ply, move, evaluation);
