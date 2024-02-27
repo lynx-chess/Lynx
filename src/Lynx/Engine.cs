57a9b0f63c8c6a1e55d0749dbfaca7e656924f21
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2024-02-27 10:59:54 +0100

    ⚡ Force GC collection at the end of `Engine` constructor and after `ucinewgame` (#685)

--- src/Lynx/Engine.cs
+++ src/Lynx/Engine.cs
@@ -69,7 +69,12 @@ public sealed partial class Engine
 
         _engineWriter = engineWriter;
         ResetEngine();
-# endif
+#endif
+
+#pragma warning disable S1215 // "GC.Collect" should not be called
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+#pragma warning restore S1215 // "GC.Collect" should not be called
     }
 
 #pragma warning disable S1144 // Unused private types or members should be removed - used in Release mode
@@ -121,6 +126,11 @@ public sealed partial class Engine
         _isNewGameCommandSupported = true;
 
         ResetEngine();
+
+#pragma warning disable S1215 // "GC.Collect" should not be called
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+#pragma warning restore S1215 // "GC.Collect" should not be called
     }
 
     public void AdjustPosition(ReadOnlySpan<char> rawPositionCommand)
