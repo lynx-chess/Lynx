2abea44bbbae25fac96a408c57a39cbd0aca6a7c
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-05-26 03:51:51 +0200

    Add custm seralizer context for `TaperedEvaluationTermByCount`

--- src/Lynx/Configuration.cs
+++ src/Lynx/Configuration.cs
@@ -429,6 +429,7 @@ public sealed class TaperedEvaluationTermByCount
 [JsonSerializable(typeof(EngineSettings))]
 [JsonSerializable(typeof(TaperedEvaluationTerm))]
 [JsonSerializable(typeof(TaperedEvaluationTermByRank))]
+[JsonSerializable(typeof(TaperedEvaluationTermByCount))]
 [JsonSerializable(typeof(SPSAAttribute<int>))]
 [JsonSerializable(typeof(SPSAAttribute<double>))]
 internal partial class EngineSettingsJsonSerializerContext : JsonSerializerContext;
