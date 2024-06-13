d7c7f282617771c1af9423d7cecf12c7a4c82b06
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-06-13 23:58:51 +0200

    Only add bonus in case of pawns on 7th rank

--- src/Lynx/Model/Position.cs
+++ src/Lynx/Model/Position.cs
@@ -922,16 +922,11 @@ public class Position
 
         var oppositeSideOffset = 6 * sameSide;
         var seventhRank = 1 + (5 * sameSide);
-        var eightRank = 7 * sameSide;
-
-        var oppositeKingSquare = PieceBitBoards[(int)Piece.K + oppositeSideOffset].GetLS1BIndex();
-        var oppositeKingRank = Constants.Rank[oppositeKingSquare];
 
         var oppositePawns = PieceBitBoards[(int)Piece.P + oppositeSideOffset];
 
         if (rookRank == seventhRank
-            && (oppositeKingRank == eightRank
-                || (oppositePawns & Masks.SeventhRankMasks[1 - sameSide]) != 0))
+            && (oppositePawns & Masks.SeventhRankMasks[1 - sameSide]) != 0)
         {
             packedBonus += Configuration.EngineSettings.SeventhRankRookBonus.PackedEvaluation;
         }
