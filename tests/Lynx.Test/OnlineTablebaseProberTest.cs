f79faa3ee4fe73a265fe181cfb10c4ac733b55bd
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2024-06-20 03:35:54 +0200

    Revert "🐛 Detect threefold repetition on `pvNode` (#796)" (#818)
    This reverts commit 49b926fc649110ea431b84643bd1cae6db797e05, which caused Lynx to draw by threefold repetition completely won endgames.

--- tests/Lynx.Test/OnlineTablebaseProberTest.cs
+++ tests/Lynx.Test/OnlineTablebaseProberTest.cs
@@ -387,7 +387,7 @@ public class OnlineTablebaseProberTest
         Assert.AreEqual("h8g7", result.BestMove.UCIString());
 
         game.MakeMove(result.BestMove);
-        Assert.True(game.IsThreefoldRepetition(false));
+        Assert.True(game.IsThreefoldRepetition());
 
         // Using local method due to async Span limitation
         static Game ParseGame()
@@ -413,7 +413,7 @@ public class OnlineTablebaseProberTest
         Assert.AreEqual("h8g7", result.BestMove.UCIString());
 
         game.MakeMove(result.BestMove);
-        Assert.True(game.IsThreefoldRepetition(false));
+        Assert.True(game.IsThreefoldRepetition());
 
         // Using local method due to async Span limitation
         static Game ParseGame()
