c8fca6ddcd9afded2489de49eb641cea991428f6
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-07-02 11:36:36 +0200

    phase > 2

--- src/Lynx/Search/NegaMax.cs
+++ src/Lynx/Search/NegaMax.cs
@@ -257,6 +257,7 @@ public sealed partial class Engine
                     // Late Move Pruning (LMP) - all quiet moves can be pruned
                     // after searching the first few given by the move ordering algorithm
                     if (depth <= Configuration.EngineSettings.LMP_MaxDepth
+                        && phase > 2
                         && moveIndex >= Configuration.EngineSettings.LMP_BaseMovesToTry + (Configuration.EngineSettings.LMP_MovesDepthMultiplier * depth)) // Based on formula suggested by Antares
                     {
                         // After making a move
