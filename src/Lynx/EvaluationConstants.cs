913759e39e9f0f1b9b772a419d20253e8a0f7065
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-08-01 14:08:16 +0200

    Fix layout

--- src/Lynx/EvaluationConstants.cs
+++ src/Lynx/EvaluationConstants.cs
@@ -18,16 +18,16 @@ public static class EvaluationConstants
 
 #pragma warning disable IDE0055 // Discard formatting in this region
 
-    public const int PSQTBucketCount = 9;
+    public const int PSQTBucketCount = 8;
 
     public static readonly int[] PSQTBucketLayout =
     [
-         7,   7,   7,   7,   8,   8,   8,   8,
-         7,   7,   7,   7,   8,   8,   8,   8,
-         4,   4,   5,   5,   5,   5,   6,   6,
-         4,   4,   5,   5,   5,   5,   6,   6,
-         4,   4,   5,   5,   5,   5,   6,   6,
-         4,   4,   4,   4,   6,   6,   6,   6,
+         6,   6,   6,   6,   7,   7,   7,   7,
+         6,   6,   6,   6,   7,   7,   7,   7,
+         3,   3,   4,   4,   4,   5,   5,   5,
+         3,   3,   4,   4,   4,   5,   5,   5,
+         3,   3,   4,   4,   4,   5,   5,   5,
+         3,   3,   3,   3,   5,   5,   5,   5,
          0,   0,   1,   1,   1,   2,   2,   2,
          0,   0,   1,   1,   1,   2,   2,   2,
     ];
