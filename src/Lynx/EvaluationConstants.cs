1489cd2e3e068f870a450bc2e340cd9e6e38a9ef
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-08-03 12:35:02 +0200

    Add schema and tuned values

--- src/Lynx/EvaluationConstants.cs
+++ src/Lynx/EvaluationConstants.cs
@@ -18,18 +18,18 @@ public static partial class EvaluationConstants
 
 #pragma warning disable IDE0055 // Discard formatting in this region
 
-    public const int PSQTBucketCount = 16;
+    public const int PSQTBucketCount = 7;
 
     public static readonly int[] PSQTBucketLayout =
     [
-         8,   9,  10,  11,  12,  13,  14,  15,
-         8,   9,  10,  11,  12,  13,  14,  15,
-         8,   9,  10,  11,  12,  13,  14,  15,
-         8,   9,  10,  11,  12,  13,  14,  15,
-         0,   1,   2,   3,   4,   5,   6,   7,
-         0,   1,   2,   3,   4,   5,   6,   7,
-         0,   1,   2,   3,   4,   5,   6,   7,
-         0,   1,   2,   3,   4,   5,   6,   7,
+         3,   3,   4,   4,   5,   5,   6,   6,
+         3,   3,   4,   4,   5,   5,   6,   6,
+         3,   3,   4,   4,   5,   5,   6,   6,
+         3,   3,   4,   4,   5,   5,   6,   6,
+         3,   3,   4,   4,   5,   5,   6,   6,
+         3,   3,   1,   1,   1,   5,   6,   6,
+         0,   0,   1,   1,   1,   2,   2,   6,
+         0,   0,   1,   1,   1,   2,   2,   2,  //
     ];
 
     public static readonly int[] GamePhaseByPiece =
