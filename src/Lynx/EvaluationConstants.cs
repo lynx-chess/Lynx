25c955477facf09a411aea8044a18f3ac7b5f890
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-08-04 22:18:45 +0200

    Add custom schema 12 - 11 reduced, that is, second rank independent as well, but grouping a1-a2 and a8-a7

--- src/Lynx/EvaluationConstants.cs
+++ src/Lynx/EvaluationConstants.cs
@@ -18,17 +18,17 @@ public static partial class EvaluationConstants
 
 #pragma warning disable IDE0055 // Discard formatting in this region
 
-    public const int PSQTBucketCount = 24;
+    public const int PSQTBucketCount = 29;
 
     public static readonly int[] PSQTBucketLayout =
     [
-        16, 17, 18, 19, 20, 21, 22, 23,
-        16, 17, 18, 19, 20, 21, 22, 23,
-        16, 17, 18, 19, 20, 21, 22, 23,
-        16, 17, 18, 19, 20, 21, 22, 23,
-        8, 9, 10, 11, 12, 13, 14, 15,
-        8, 9, 10, 11, 12, 13, 14, 15,
-        8, 9, 10, 11, 12, 13, 14, 15,
+        21, 22, 23, 24, 25, 26, 27, 28,
+        21, 22, 23, 24, 25, 26, 27, 28,
+        21, 22, 23, 24, 25, 26, 27, 28,
+        21, 22, 23, 24, 25, 26, 27, 28,
+        14, 15, 16, 17, 18, 19, 20, 7,
+        14, 15, 16, 17, 18, 19, 20, 7,
+        0, 8 ,9, 10, 11, 12, 13, 7,
         0, 1, 2, 3, 4, 5, 6, 7, //
     ];
 
