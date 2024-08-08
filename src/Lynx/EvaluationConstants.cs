0e2aeaf487f6d41c9cdfab9afb6c25ee267838a3
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-08-08 20:25:07 +0200

    Revert undesired changes

--- src/Lynx/EvaluationConstants.cs
+++ src/Lynx/EvaluationConstants.cs
@@ -18,18 +18,18 @@ public static partial class EvaluationConstants
 
 #pragma warning disable IDE0055 // Discard formatting in this region
 
-    public const int PSQTBucketCount = 16;
+    public const int PSQTBucketCount = 23;
 
     public static readonly int[] PSQTBucketLayout =
     [
-         8,   9,  10,  11,  12,  13,  14,  15,
-         8,   9,  10,  11,  12,  13,  14,  15,
-         8,   9,  10,  11,  12,  13,  14,  15,
-         8,   9,  10,  11,  12,  13,  14,  15,
-         0,   1,   2,   3,   4,   5,   6,   7,
-         0,   1,   2,   3,   4,   5,   6,   7,
-         0,   1,   2,   3,   4,   5,   6,   7,
-         0,   1,   2,   3,   4,   5,   6,   7,
+        15, 16, 17, 18, 19, 20, 21, 22,
+        15, 16, 17, 18, 19, 20, 21, 22,
+        15, 16, 17, 18, 19, 20, 21, 22,
+        15, 16, 17, 18, 19, 20, 21, 22,
+        15, 8, 9, 10, 11, 12, 13, 14,
+        15, 8, 9, 10, 11, 12, 13, 14,
+        0, 8, 9, 10, 11, 12, 13, 14,
+        0, 1, 2, 3, 4, 5, 6, 7, //
     ];
 
     public static readonly int[] GamePhaseByPiece =
