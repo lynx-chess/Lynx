250d951b1e268460de7ed121756c035f827fec9f
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-08-17 14:00:44 +0200

    Update `ArrayCopy_Benchmark` with our use case

--- src/Lynx.Benchmark/ArrayCopy_Benchmark.cs
+++ src/Lynx.Benchmark/ArrayCopy_Benchmark.cs
@@ -23,10 +23,10 @@ using BenchmarkDotNet.Attributes;
 namespace Lynx.Benchmark;
 public class ArrayCopy_Benchmark : BaseBenchmark
 {
-    public static IEnumerable<ulong[]> Data => new List<ulong[]> {
-        new ulong[]{ 1, 2, 3 },
-        new ulong[]{ 1, 2, 3, 4, 5, 6 ,7 ,8 ,9 ,10, 11, 12 },
-        new ulong[]{
+    public static IEnumerable<int[]> Data => new List<int[]> {
+        new int[]{ 1, 2, 3 },
+        new int[]{ 1, 2, 3, 4, 5, 6 ,7 ,8 ,9 ,10, 11, 12 },
+        new int[]{
             01, 02, 03, 04, 05, 06 ,07 ,08 ,09 ,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
             21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
             41, 42, 43, 44, 45, 45, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58 ,59 ,60,
@@ -34,6 +34,7 @@ public class ArrayCopy_Benchmark : BaseBenchmark
             81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 00,
             01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15 ,16, 17 ,18, 19, 20,
             21, 22, 23, 24 },
+        EvaluationConstants.PackedPSQT
     };
 
     [Benchmark(Baseline = true)]
