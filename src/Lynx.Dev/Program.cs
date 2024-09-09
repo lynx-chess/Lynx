24604bb5f1ca0e9646b0b84854e2f961f15b7fd3
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-09-09 18:32:50 +0200

    Cleanup

--- src/Lynx.Dev/Program.cs
+++ src/Lynx.Dev/Program.cs
@@ -1080,7 +1080,7 @@ static void TranspositionTable()
     var hashKey2 = position.UniqueIdentifier & mask;
     Console.WriteLine(hashKey2);
 
-    transpositionTable.ClearTranspositionTable();
+    Array.Clear(transpositionTable);
 
     //transpositionTable.RecordHash(position, depth: 3, maxDepth: 5, move: 1234, eval: +5, nodeType: NodeType.Alpha);
     //var entry = transpositionTable.ProbeHash(position, maxDepth: 5, depth: 3, alpha: 1, beta: 2);
