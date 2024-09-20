1dd0493644dd0040242feb25315b4d1ca99f3cfc
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-09-20 20:38:11 +0200

    Move alternative impl to comment

--- src/Lynx.Cli/Listener.cs
+++ src/Lynx.Cli/Listener.cs
@@ -50,23 +50,19 @@ public sealed class Listener
     /// Because this includes the Environment.NewLine character(s), the method can read lines that contain up to 254 characters.
     /// To read longer lines, call the OpenStandardInput(Int32) method.
     /// Source: https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=net-8.0
+    /// Something like this would work as well to read input without the input buffer limitation:
+    ///    private static string ReadInput()
+    ///    {
+    ///        Span<byte> bytes = stackalloc byte[4096 * 4];
+    ///        Stream inputStream = Console.OpenStandardInput();
+    ///        int outputLength = inputStream.Read(bytes);
+    ///
+    ///        return Encoding.UTF8.GetString(bytes[..outputLength]);
+    ///    }
     /// </summary>
     private static void IncreaseInputBufferSize()
     {
         // Based on Lizard's solution. 4096 * 4 is enough to accept position commands with at least 500 moves
         Console.SetIn(new StreamReader(Console.OpenStandardInput(), Encoding.UTF8, false, 4096 * 4));
-    }
-
-    /// <summary>
-    /// Something like this would work as well to read input without the input buffer limitation
-    /// </summary>
-    /// <returns></returns>
-    private static string  ReadInput()
-    {
-        Span<byte> bytes = stackalloc byte[4096 * 4];
-        Stream inputStream = Console.OpenStandardInput();
-        int outputLength = inputStream.Read(bytes);
-
-        return Encoding.UTF8.GetString(bytes[..outputLength]);
     }
 }
