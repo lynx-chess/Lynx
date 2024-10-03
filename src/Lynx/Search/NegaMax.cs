ad5a3a62be86284d6bcfe9e694c641e8263e65f5
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-10-03 23:03:07 +0200

    Threshold -> -100 * depth

--- src/Lynx/Search/NegaMax.cs
+++ src/Lynx/Search/NegaMax.cs
@@ -294,7 +294,7 @@ public sealed partial class Engine
                     }
 
                     // 🔍 SEE pruning
-                    if (!SEE.HasPositiveScore(position, move, threshold: -1_000))
+                    if (!SEE.HasPositiveScore(position, move, threshold: -100 * depth))
                     {
                         // After making a move
                         Game.HalfMovesWithoutCaptureOrPawnMove = oldHalfMovesWithoutCaptureOrPawnMove;
