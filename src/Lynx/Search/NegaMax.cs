b19159a5220b4dc9fc035b1d1bc06412555f16d7
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2024-10-29 11:30:33 +0100

    🔍 History: increase bonus when static eval is lower than alpha  (#1123)

--- src/Lynx/Search/NegaMax.cs
+++ src/Lynx/Search/NegaMax.cs
@@ -397,6 +397,11 @@ public sealed partial class Engine
 
                     var historyDepth = depth;
 
+                    if (staticEval <= alpha)
+                    {
+                        ++historyDepth;
+                    }
+
                     // Suggestion by Sirius author
                     if (bestScore >= beta + Configuration.EngineSettings.History_BestScoreBetaMargin)
                     {
