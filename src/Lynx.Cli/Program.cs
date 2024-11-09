2add9fbc5f58063a86ffa9a56d024636aa9b8621
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-11-09 02:50:21 +0100

    Move Lynx init logic to Runner

--- src/Lynx.Cli/Program.cs
+++ src/Lynx.Cli/Program.cs
@@ -1,11 +1,8 @@
 ﻿using Lynx;
 using Lynx.Cli;
-using Lynx.Model;
-using Lynx.UCI.Commands.Engine;
 using Microsoft.Extensions.Configuration;
 using NLog;
 using NLog.Extensions.Logging;
-using System.Threading.Channels;
 
 #if DEBUG
 Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
@@ -27,55 +24,6 @@ if (Configuration.GeneralSettings.EnableLogging)
     LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
 }
 
-var uciChannel = Channel.CreateBounded<string>(new BoundedChannelOptions(100) { SingleReader = true, SingleWriter = true, FullMode = BoundedChannelFullMode.Wait });
-var engineChannel = Channel.CreateBounded<object>(new BoundedChannelOptions(2 * Configuration.EngineSettings.MaxDepth) { SingleReader = true, SingleWriter = false, FullMode = BoundedChannelFullMode.DropOldest });
-
-using CancellationTokenSource source = new();
-CancellationToken cancellationToken = source.Token;
-
-var tt = new TranspositionTable();
-var engine = new Engine(engineChannel, tt);
-var uciHandler = new UCIHandler(uciChannel, engineChannel, engine);
-
-var tasks = new List<Task>
-{
-    Task.Run(() => new Writer(engineChannel).Run(cancellationToken)),
-    Task.Run(() => new Searcher(uciChannel, engineChannel, engine, tt).Run(cancellationToken)),
-    Task.Run(() => new Listener(uciHandler).Run(cancellationToken, args)),
-    uciChannel.Reader.Completion,
-    engineChannel.Reader.Completion
-};
-
-try
-{
-    Console.WriteLine($"{IdCommand.EngineName} {IdCommand.GetVersion()} by {IdCommand.EngineAuthor}");
-    await Task.WhenAny(tasks);
-}
-catch (AggregateException ae)
-{
-    foreach (var e in ae.InnerExceptions)
-    {
-        if (e is TaskCanceledException taskCanceledException)
-        {
-            Console.WriteLine("Cancellation requested: {0}", taskCanceledException.Message);
-        }
-        else
-        {
-            Console.WriteLine("Exception: {0}", e.GetType().Name);
-        }
-    }
-}
-catch (Exception e)
-{
-    Console.WriteLine("Unexpected exception");
-    Console.WriteLine(e.Message);
-}
-finally
-{
-    engineChannel.Writer.TryComplete();
-    uciChannel.Writer.TryComplete();
-    //source.Cancel();
-    LogManager.Shutdown(); // Flush and close down internal threads and timers
-}
+await Runner.Run(args);
 
 Thread.Sleep(2_000);
---
+++ src/Lynx.Cli/Runner.cs
@@ -0,0 +1,69 @@
+﻿using Lynx.Model;
+using Lynx.UCI.Commands.Engine;
+using NLog;
+using System.Threading.Channels;
+
+namespace Lynx.Cli;
+
+public static class Runner
+{
+    private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+    public static async Task Run(params string[] args)
+    {
+        var uciChannel = Channel.CreateBounded<string>(new BoundedChannelOptions(100) { SingleReader = true, SingleWriter = true, FullMode = BoundedChannelFullMode.Wait });
+        var engineChannel = Channel.CreateBounded<object>(new BoundedChannelOptions(2 * Configuration.EngineSettings.MaxDepth) { SingleReader = true, SingleWriter = false, FullMode = BoundedChannelFullMode.DropOldest });
+
+        using CancellationTokenSource source = new();
+        CancellationToken cancellationToken = source.Token;
+
+        var tt = new TranspositionTable();
+        var engine = new Engine(engineChannel, tt);
+        var uciHandler = new UCIHandler(uciChannel, engineChannel, engine);
+
+        var tasks = new List<Task>
+        {
+            Task.Run(() => new Writer(engineChannel).Run(cancellationToken)),
+            Task.Run(() => new Searcher(uciChannel, engineChannel, engine, tt).Run(cancellationToken)),
+            Task.Run(() => new Listener(uciHandler).Run(cancellationToken, args)),
+            uciChannel.Reader.Completion,
+            engineChannel.Reader.Completion
+        };
+
+        try
+        {
+            Console.WriteLine($"{IdCommand.EngineName} {IdCommand.GetVersion()} by {IdCommand.EngineAuthor}");
+            await Task.WhenAny(tasks);
+        }
+        catch (AggregateException ae)
+        {
+            foreach (var e in ae.InnerExceptions)
+            {
+                if (e is TaskCanceledException taskCanceledException)
+                {
+                    Console.WriteLine("Cancellation requested exception: {0}", taskCanceledException.Message);
+                    _logger.Fatal(ae, "Cancellation requested exception: {0}", taskCanceledException.Message);
+                }
+                else
+                {
+                    Console.WriteLine("Exception {0}: {1}", e.GetType().Name, e.Message);
+                    _logger.Fatal(ae, "Exception {0}: {1}", e.GetType().Name, e.Message);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Unexpected exception");
+            Console.WriteLine(e.Message);
+
+            _logger.Fatal(e, "Unexpected exception: {Exception}", e.Message);
+        }
+        finally
+        {
+            engineChannel.Writer.TryComplete();
+            uciChannel.Writer.TryComplete();
+            //source.Cancel();
+            LogManager.Shutdown(); // Flush and close down internal threads and timers
+        }
+    }
+}
