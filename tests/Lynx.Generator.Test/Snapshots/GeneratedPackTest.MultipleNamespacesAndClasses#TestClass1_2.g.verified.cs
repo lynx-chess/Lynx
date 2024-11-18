f9cd9abfbf16848d3402e8e824fa7c3eac954f77
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-11-18 15:28:16 +0100

    Add tests for GeneratedPack sourcegen, multiple namespaces and classes

---
+++ tests/Lynx.Generator.Test/Snapshots/GeneratedPackTest.MultipleNamespacesAndClasses#TestClass1_2.g.verified.cs
@@ -0,0 +1,7 @@
+﻿//HintName: TestClass1_2.g.cs
+namespace Lynx.Generator.Namespace1;
+
+static partial class TestClass1_2
+{
+    public const int TestConstant1_2 = 196610;                  // Pack(   2,    3)
+}
---
+++ tests/Lynx.Generator.Test/Snapshots/GeneratedPackTest.MultipleNamespacesAndClasses#TestClass2_1.g.verified.cs
@@ -0,0 +1,7 @@
+﻿//HintName: TestClass2_1.g.cs
+namespace Lynx.Generator.Namespace2;
+
+static partial class TestClass2_1
+{
+    public const int TestConstant2_1 = 327684;                  // Pack(   4,    5)
+}
