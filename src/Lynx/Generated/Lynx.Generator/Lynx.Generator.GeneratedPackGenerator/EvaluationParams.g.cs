25cb75126872a11f3ce42e35acfc6542ef108bd0
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-11-18 01:00:52 +0100

    Save our own source generator output in git https://andrewlock.net/creating-a-source-generator-part-6-saving-source-generator-output-in-source-control/

---
+++ src/Lynx/Generated/Lynx.Generator/Lynx.Generator.GeneratedPackGenerator/EvaluationParams.g.cs
@@ -0,0 +1,14 @@
+﻿namespace Lynx;
+
+static partial class EvaluationParams
+{
+    public const int IsolatedPawnPenalty = -917521;
+    public const int OpenFileRookBonus = 131112;
+    public const int SemiOpenFileRookBonus = 524303;
+    public const int SemiOpenFileKingPenalty = 327656;
+    public const int OpenFileKingPenalty = 131007;
+    public const int KingShieldBonus = -196585;
+    public const int BishopPairBonus = 4718622;
+    public const int PieceProtectedByPawnBonus = 983052;
+    public const int PieceAttackedByPawnPenalty = -2162735;
+}
