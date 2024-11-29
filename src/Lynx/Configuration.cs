68b4859245eeb6eadd27521335a4fe0d4aaad169
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-11-29 12:57:08 +0100

    Add tuned tm params ``` iterations: 412 (289.32s per iter) games: 13184 (9.04s per game) HardTimeBoundMultiplier = 61(+8.93) in [10, 100] SoftTimeBoundMultiplier = 102(+2.15) in [50, 150] SoftTimeBaseIncrementMultiplier = 67(-13.174505) in [10, 200] NodeTmBase = 247(-2.879693) in [100, 300] NodeTmScale = 161(+10.69) in [50, 250] ```

--- src/Lynx/Configuration.cs
+++ src/Lynx/Configuration.cs
@@ -103,19 +103,19 @@ public sealed class EngineSettings
     #region Time management
 
     [SPSA<double>(0.1, 1, 0.05)]
-    public double HardTimeBoundMultiplier { get; set; } = 0.52;
+    public double HardTimeBoundMultiplier { get; set; } = 0.61;
 
     [SPSA<double>(0.5, 1.5, 0.05)]
-    public double SoftTimeBoundMultiplier { get; set; } = 0.97;
+    public double SoftTimeBoundMultiplier { get; set; } = 1.02;
 
     [SPSA<double>(0.1, 2, 0.1)]
-    public double SoftTimeBaseIncrementMultiplier { get; set; } = 0.6;
+    public double SoftTimeBaseIncrementMultiplier { get; set; } = 0.67;
 
     [SPSA<double>(1, 3, 0.1)]
-    public double NodeTmBase { get; set; } = 2.53;
+    public double NodeTmBase { get; set; } = 2.47;
 
     [SPSA<double>(0.5, 2.5, 0.1)]
-    public double NodeTmScale { get; set; } = 1.63;
+    public double NodeTmScale { get; set; } = 1.61;
 
     #endregion
 
