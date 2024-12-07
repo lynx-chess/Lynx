7fb1ac861c51fed51eab606e3b6e64a3160580b6
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-12-07 05:28:48 +0100

    Remove MultiThreadedSearch implementation

--- src/Lynx/Searcher.cs
+++ src/Lynx/Searcher.cs
@@ -83,7 +83,7 @@ public sealed class Searcher
         }
         else
         {
-            await MultiThreadedSearch(goCommand);
+            MultiThreadedSearch(goCommand);
         }
     }
 
@@ -99,69 +99,9 @@ public sealed class Searcher
         }
     }
 
-    private async Task MultiThreadedSearch(GoCommand goCommand)
+    private void MultiThreadedSearch(GoCommand goCommand)
     {
-        var searchConstraints = TimeManager.CalculateTimeManagement(_mainEngine.Game, goCommand);
-
-#if MULTITHREAD_DEBUG
-        var sw = Stopwatch.StartNew();
-        var lastElapsed = sw.ElapsedMilliseconds;
-#endif
-
-        // Lazy SMP implementation
-
-        // Extra engines run in "go infinite" mode
-        // TODO: fix pondering, since pondering logic updated the boolean in the constraint object instead of a field
-        var extraEnginesSearchConstraint = SearchConstraints.InfiniteSearchConstraint;
-
-        var tasks = _extraEngines
-            .Select(engine =>
-                Task.Run(() => engine.Search(goCommand, extraEnginesSearchConstraint)))
-            //taskFactory.StartNew(() => engine.Search(goCommand, extraEnginesSearchConstraint)))
-            .ToArray();
-
-#if MULTITHREAD_DEBUG
-        _logger.Warn("End of extra searches prep, {0} ms", sw.ElapsedMilliseconds - lastElapsed);
-        lastElapsed = sw.ElapsedMilliseconds;
-#endif
-
-        SearchResult? finalSearchResult = _mainEngine.Search(goCommand, searchConstraints);
-
-#if MULTITHREAD_DEBUG
-        _logger.Warn("End of main search, {0} ms", sw.ElapsedMilliseconds - lastElapsed);
-        lastElapsed = sw.ElapsedMilliseconds;
-#endif
-
-        foreach (var engine in _extraEngines)
-        {
-            engine.StopSearching();
-        }
-
-        var extraResults = await Task.WhenAll(tasks);
-
-#if MULTITHREAD_DEBUG
-        _logger.Warn("End of extra searches, {0} ms", sw.ElapsedMilliseconds - lastElapsed);
-#endif
-
-        if (finalSearchResult is not null)
-        {
-            foreach (var extraResult in extraResults)
-            {
-                finalSearchResult.Nodes += extraResult?.Nodes ?? 0;
-            }
-
-            finalSearchResult.NodesPerSecond = Utils.CalculateNps(finalSearchResult.Nodes, 0.001 * finalSearchResult.Time);
-
-#if MULTITHREAD_DEBUG
-            _logger.Warn("End of multithread calculations, {0} ms", sw.ElapsedMilliseconds - lastElapsed);
-#endif
-
-            // Final info command
-            _engineWriter.TryWrite(finalSearchResult);
-
-            // We always print best move, even in case of go ponder + stop, in which case IDEs are expected to ignore it
-            _engineWriter.TryWrite(new BestMoveCommand(finalSearchResult));
-        }
+        SingleThreadedSearch(goCommand);
     }
 
     public void AdjustPosition(ReadOnlySpan<char> command)
