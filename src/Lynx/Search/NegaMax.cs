b7ded183a25f8ec28452260f42df039cfe532d47
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-12-08 17:30:29 +0100

    Why not, let's try

--- src/Lynx/Search/NegaMax.cs
+++ src/Lynx/Search/NegaMax.cs
@@ -213,6 +213,16 @@ public sealed partial class Engine
             }
         }
 
+        // 🔍 IIR again - the return of the nonsense
+        // Jokes aside, idea seen in Plentychess but heavily tweaked
+        if (!isRoot
+            && pvNode
+            && depth >= Configuration.EngineSettings.IIR_MinDepth
+            && ttBestMove == default)
+        {
+            --depth;
+        }
+
         Span<Move> moves = stackalloc Move[Constants.MaxNumberOfPossibleMovesInAPosition];
         var pseudoLegalMoves = MoveGenerator.GenerateAllMoves(position, moves);
 
