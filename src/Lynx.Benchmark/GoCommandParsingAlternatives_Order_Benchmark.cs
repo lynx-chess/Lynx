2b783f3a323e7c060dd1e0b60b17e3728c565167
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-12-09 07:29:48 +0100

    Try parsing multiple times and without unneeded async

--- src/Lynx.Benchmark/GoCommandParsingAlternatives_Order_Benchmark.cs
+++ src/Lynx.Benchmark/GoCommandParsingAlternatives_Order_Benchmark.cs
@@ -1,4 +1,28 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿/*
+ *
+BenchmarkDotNet v0.14.0, Ubuntu 22.04.5 LTS (Jammy Jellyfish)
+AMD EPYC 7763, 1 CPU, 4 logical and 2 physical cores
+.NET SDK 9.0.101
+  [Host]     : .NET 9.0.0 (9.0.24.52809), X64 RyuJIT AVX2
+  DefaultJob : .NET 9.0.0 (9.0.24.52809), X64 RyuJIT AVX2
+| Method      | command              | Mean     | Error    | StdDev   | Ratio | RatioSD | Gen0   | Allocated | Alloc Ratio |
+|------------ |--------------------- |---------:|---------:|---------:|------:|--------:|-------:|----------:|------------:|
+| Naive       | go bi(...)onder [49] | 923.3 ns | 14.84 ns | 13.88 ns |  1.00 |    0.02 | 0.0019 |     272 B |        1.00 |
+| Wtime_First | go bi(...)onder [49] | 888.8 ns |  5.96 ns |  5.58 ns |  0.96 |    0.02 | 0.0029 |     272 B |        1.00 |
+|             |                      |          |          |          |       |         |        |           |             |
+| Naive       | go po(...)c 500 [49] | 892.1 ns |  9.54 ns |  8.46 ns |  1.00 |    0.01 | 0.0019 |     272 B |        1.00 |
+| Wtime_First | go po(...)c 500 [49] | 907.3 ns | 11.62 ns | 10.87 ns |  1.02 |    0.02 | 0.0019 |     272 B |        1.00 |
+|             |                      |          |          |          |       |         |        |           |             |
+| Naive       | go wt(...)c 500 [42] | 905.7 ns | 12.89 ns | 12.06 ns |  1.00 |    0.02 | 0.0019 |     272 B |        1.00 |
+| Wtime_First | go wt(...)c 500 [42] | 951.4 ns | 12.96 ns | 12.12 ns |  1.05 |    0.02 | 0.0019 |     272 B |        1.00 |
+|             |                      |          |          |          |       |         |        |           |             |
+| Naive       | go wt(...)onder [49] | 916.4 ns | 11.50 ns | 10.76 ns |  1.00 |    0.02 | 0.0019 |     272 B |        1.00 |
+| Wtime_First | go wt(...)onder [49] | 908.7 ns | 13.53 ns | 12.66 ns |  0.99 |    0.02 | 0.0029 |     272 B |        1.00 |
+ *
+ */
+
+using BenchmarkDotNet.Attributes;
+using Microsoft.Diagnostics.Utilities;
 using NLog;
 
 namespace Lynx.Benchmark;
@@ -10,6 +34,12 @@ public class GoCommandParsingAlternatives_Order_Benchmark : BaseBenchmark
 {
     private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
+    [Params(1, 10, 100, 1000)]
+    public int Count { get; set; }
+
+    [ParamsSource(nameof(Data))]
+    public string? Command { get; set; }
+
     public static IEnumerable<string> Data =>
     [
         "go wtime 7000 winc 500 btime 8000 binc 500",
@@ -19,17 +49,25 @@ public class GoCommandParsingAlternatives_Order_Benchmark : BaseBenchmark
     ];
 
     [Benchmark(Baseline = true)]
-    [ArgumentsSource(nameof(Data))]
-    public async Task Naive(string command)
+    public void Naive()
     {
-        await Task.Run(() => ParseNaive(command));
+        var command = Command!;
+
+        for (int i = 0; i < Count; ++i)
+        {
+            ParseNaive(command);
+        }
     }
 
     [Benchmark]
-    [ArgumentsSource(nameof(Data))]
-    public async Task Wtime_First(string command)
+    public void Wtime_First()
     {
-        await Task.Run(() => ParseWtime_First(command));
+        var command = Command!;
+
+        for (int i = 0; i < Count; ++i)
+        {
+            ParseWtime_First(command);
+        }
     }
 
     public List<string> SearchMoves { get; private set; } = default!;
