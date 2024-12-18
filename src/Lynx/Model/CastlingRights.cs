73e63b725ce264f5a607fd9b62ba62d0241b269c
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2024-12-18 17:44:56 +0100

    Use Flags attribute for CastlingRights

--- src/Lynx/Model/CastlingRights.cs
+++ src/Lynx/Model/CastlingRights.cs
@@ -10,6 +10,7 @@
 /// * 1111      Both sides can castle both directions
 /// * 1001      Black king => only O-O-O; White king => only O-O
 /// </summary>
+[Flags]
 public enum CastlingRights : byte
 {
     WK = 1, WQ = 2, BK = 4, BQ = 8
