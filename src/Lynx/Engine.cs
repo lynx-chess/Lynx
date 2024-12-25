lynx-chess/Lynx

commit b45a7f18c867fdbb63bef186a9e51325446b342c

🧹 Log thread ids (#1294)
* Add engine id to most warning and some info logs, reserving `MULTITHREAD_DEBUG` for debug commands or UCI output from threads

src/Lynx/Engine.cs
@@ -265,11 +265,7 @@ public sealed partial class Engine : IDisposable
         }
         catch (Exception e)
         {
-            _logger.Fatal(e,
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                "Error in {0} for position {Position}", nameof(Search), Game.CurrentPosition.FEN());
+            _logger.Fatal(e, "[#{EngineId}] Error in {Method} for position {Position}", _id, nameof(Search), Game.CurrentPosition.FEN());
             return null;
         }
         finally

src/Lynx/Search/IDDFS.cs
@@ -126,11 +126,8 @@ public sealed partial class Engine
                     beta = Math.Clamp(lastSearchResult.Score + window, EvaluationConstants.MinEval, EvaluationConstants.MaxEval);
 
                     _logger.Info(
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                        "Depth {Depth}: aspiration windows [{Alpha}, {Beta}] for previous search score {Score}, nodes {Nodes}",
-                        depth, alpha, beta, lastSearchResult.Score, _nodes);
+                        "[#{EngineId}] Depth {Depth}: aspiration windows [{Alpha}, {Beta}] for previous search score {Score}, nodes {Nodes}",
+                        _id, depth, alpha, beta, lastSearchResult.Score, _nodes);
                     Debug.Assert(lastSearchResult.Mate == 0 && lastSearchResult.Score > EvaluationConstants.NegativeCheckmateDetectionLimit && lastSearchResult.Score < EvaluationConstants.PositiveCheckmateDetectionLimit);
 
                     while (true)
@@ -139,11 +136,8 @@ public sealed partial class Engine
                         Debug.Assert(depthToSearch > 0);
 
                         _logger.Info(
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                            "Aspiration windows depth {Depth} ({DepthWithoutReduction} - {Reduction}), window {Window}: [{Alpha}, {Beta}] for score {Score}, nodes {Nodes}",
-                            depthToSearch, depth, failHighReduction, window, alpha, beta, bestScore, _nodes);
+                            "[#{EngineId}] Aspiration windows depth {Depth} ({DepthWithoutReduction} - {Reduction}), window {Window}: [{Alpha}, {Beta}] for score {Score}, nodes {Nodes}",
+                            _id, depthToSearch, depth, failHighReduction, window, alpha, beta, bestScore, _nodes);
 
                         bestScore = NegaMax(depth: depthToSearch, ply: 0, alpha, beta, cutnode: false, cancellationToken);
                         Debug.Assert(bestScore > EvaluationConstants.MinEval && bestScore < EvaluationConstants.MaxEval);
@@ -174,11 +168,8 @@ public sealed partial class Engine
                         if (bestScore > EvaluationConstants.CheckMateBaseEvaluation)
                         {
                             _logger.Warn(
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                                "Depth {Depth}: potential +X checkmate detected in position {Position}, but score {BestScore} outside of the limits",
-                                depth, Game.PositionBeforeLastSearch.FEN(), bestScore);
+                                "[#{EngineId}] Depth {Depth}: potential +X checkmate detected in position {Position}, but score {BestScore} outside of the limits",
+                                _id, depth, Game.PositionBeforeLastSearch.FEN(), bestScore);
 
                             bestScore = EvaluationConstants.PositiveCheckmateDetectionLimit + 1;
 
@@ -188,11 +179,8 @@ public sealed partial class Engine
                         if (bestScore < -EvaluationConstants.CheckMateBaseEvaluation)
                         {
                             _logger.Warn(
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                                "Depth {Depth}: potential -X checkmate detected in position {Position}, but score {BestScore} outside of the limits",
-                                depth, Game.PositionBeforeLastSearch.FEN(), bestScore);
+                                "[#{EngineId}] Depth {Depth}: potential -X checkmate detected in position {Position}, but score {BestScore} outside of the limits",
+                                _id, depth, Game.PositionBeforeLastSearch.FEN(), bestScore);
 
                             bestScore = EvaluationConstants.NegativeCheckmateDetectionLimit - 1;
 
@@ -218,11 +206,8 @@ public sealed partial class Engine
                 if (lastSearchResultCandidate.BestMove == default)
                 {
                     _logger.Warn(
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                        "Depth {Depth}: search didn't produce a best move for position {Position}. Score {Score} (mate in {Mate}?) detected",
-                        depth, Game.PositionBeforeLastSearch.FEN(), bestScore, mate);
+                        "[#{EngineId}] Depth {Depth}: search didn't produce a best move for position {Position}. Score {Score} (mate in {Mate}?) detected",
+                        _id, depth, Game.PositionBeforeLastSearch.FEN(), bestScore, mate);
 
                     _bestMoveStability = 0;
                     _scoreDelta = 0;
@@ -250,10 +235,8 @@ public sealed partial class Engine
         {
 #pragma warning disable S6667 // Logging in a catch clause should pass the caught exception as a parameter - expected exception we want to ignore
             _logger.Info(
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                "Depth {Depth}: search cancellation requested after {Time}ms (nodes {Nodes}), best move will be returned", depth, _stopWatch.ElapsedMilliseconds, _nodes);
+                "[#{EngineId}] Depth {Depth}: search cancellation requested after {Time}ms (nodes {Nodes}), best move will be returned",
+                _id, depth, _stopWatch.ElapsedMilliseconds, _nodes);
 #pragma warning restore S6667 // Logging in a catch clause should pass the caught exception as a parameter.
 
             for (int i = 0; i < lastSearchResult?.Moves.Length; ++i)
@@ -264,11 +247,8 @@ public sealed partial class Engine
         catch (Exception e) when (e is not LynxException)
         {
             _logger.Error(e,
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                "Depth {Depth}: unexpected error ocurred during the search of position {Position}, best move will be returned\n{StackTrace}",
-                depth, Game.PositionBeforeLastSearch.FEN(), e.StackTrace);
+                "[#{EngineId}] Depth {Depth}: unexpected error ocurred during the search of position {Position}, best move will be returned\n{StackTrace}",
+                _id, depth, Game.PositionBeforeLastSearch.FEN(), e.StackTrace);
         }
         finally
         {
@@ -292,10 +272,7 @@ public sealed partial class Engine
         if (bestMove is null || bestMove == 0)
         {
             _logger.Warn(
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                "Depth {Depth}: search continues, due to lack of best move", depth - 1);
+                "[#{EngineId}] Depth {Depth}: search continues, due to lack of best move", _id, depth - 1);
 
             return true;
         }
@@ -305,44 +282,32 @@ public sealed partial class Engine
             if (mate == EvaluationConstants.MaxMate || mate == EvaluationConstants.MinMate)
             {
                 _logger.Warn(
-#if MULTITHREAD_DEBUG
-                    $"[#{_id}] " +
-#endif
-                    "Depth {Depth}: mate outside of range detected, stopping search and playing best move {BestMove}",
-                    depth - 1, bestMove.Value.UCIString());
+                    "[#{EngineId}] Depth {Depth}: mate outside of range detected, stopping search and playing best move {BestMove}",
+                    _id, depth - 1, bestMove.Value.UCIString());
 
                 return false;
             }
 
             var winningMateThreshold = (100 - Game.HalfMovesWithoutCaptureOrPawnMove) / 2;
             _logger.Info(
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                "Depth {Depth}: mate in {Mate} detected (score {Score}, {MateThreshold} moves until draw by repetition)",
-                depth - 1, mate, bestScore, winningMateThreshold);
+                "[#{EngineId}] Depth {Depth}: mate in {Mate} detected (score {Score}, {MateThreshold} moves until draw by repetition)",
+                _id, depth - 1, mate, bestScore, winningMateThreshold);
 
             if (mate < 0 || mate + Constants.MateDistanceMarginToStopSearching < winningMateThreshold)
             {
-                _logger.Info(
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                    "Stopping search, mate is short enough");
+                _logger.Info("[#{EngineId}] Stopping search, mate is short enough", _id);
 
                 return false;
             }
 
-            _logger.Info(
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                "Search continues, hoping to find a faster mate");
+            _logger.Info("[#{EngineId}] Search continues, hoping to find a faster mate", _id);
         }
 
         if (depth >= Configuration.EngineSettings.MaxDepth)
         {
-            _logger.Info("Max depth reached: {MaxDepth}", Configuration.EngineSettings.MaxDepth);
+            _logger.Info(
+                "[#{EngineId}] Max depth reached: {MaxDepth}",
+                _id, Configuration.EngineSettings.MaxDepth);
             return false;
         }
 
@@ -353,7 +318,7 @@ public sealed partial class Engine
 
             if (!shouldContinue)
             {
-                _logger.Info("Depth {Depth}: stopping, max. depth reached", depth - 1);
+                _logger.Info("[#{EngineId}] Depth {Depth}: stopping, max. depth reached", _id, depth - 1);
             }
 
             return shouldContinue;
@@ -366,18 +331,14 @@ public sealed partial class Engine
             var bestMoveNodeCount = _moveNodeCount[bestMove.Value.Piece()][bestMove.Value.TargetSquare()];
             var scaledSoftLimitTimeBound = TimeManager.SoftLimit(_searchConstraints, depth - 1, bestMoveNodeCount, _nodes, _bestMoveStability, _scoreDelta);
             _logger.Debug(
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                "[TM] Depth {Depth}: hard limit {HardLimit}, base soft limit {BaseSoftLimit}, scaled soft limit {ScaledSoftLimit}", depth - 1, _searchConstraints.HardLimitTimeBound, _searchConstraints.SoftLimitTimeBound, scaledSoftLimitTimeBound);
+                "[#{EngineId}] [TM] Depth {Depth}: hard limit {HardLimit}, base soft limit {BaseSoftLimit}, scaled soft limit {ScaledSoftLimit}",
+                _id, depth - 1, _searchConstraints.HardLimitTimeBound, _searchConstraints.SoftLimitTimeBound, scaledSoftLimitTimeBound);
 
             if (elapsedMilliseconds > scaledSoftLimitTimeBound)
             {
                 _logger.Info(
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                    "[TM] Stopping at depth {0} (nodes {1}): {2}ms > {3}ms", depth - 1, _nodes, elapsedMilliseconds, scaledSoftLimitTimeBound);
+                    "[#{EngineId}] [TM] Stopping at depth {0} (nodes {1}): {2}ms > {3}ms",
+                    _id, depth - 1, _nodes, elapsedMilliseconds, scaledSoftLimitTimeBound);
                 return false;
             }
         }
@@ -480,10 +441,7 @@ public sealed partial class Engine
         if (lastSearchResult is null)
         {
             var noDepth1Message =
-#if MULTITHREAD_DEBUG
-                $"[#{_id}] " +
-#endif
-                $"Depth {depth}: search cancelled with no result for position {Game.CurrentPosition.FEN()} (hard limit {_searchConstraints.HardLimitTimeBound}ms, soft limit {_searchConstraints.SoftLimitTimeBound}ms). Choosing first found legal move as best one";
+                $"[#{_id}] Depth {depth}: search cancelled with no result for position {Game.CurrentPosition.FEN()} (hard limit {_searchConstraints.HardLimitTimeBound}ms, soft limit {_searchConstraints.SoftLimitTimeBound}ms). Choosing first found legal move as best one";
 
             // In the event of a quick ponderhit/stop while pondering because the opponent moved quickly, we don't want no warning triggered here
             //  when cancelling the pondering search

commit 7c19a030bc8db01b0b714228df7fffdaae55200e

🧹 Make `Engine` id an integer (#1297)
Thread id logging was somehow broken by #1294, since double quotes were using for the id.
This addresses this issue and makes ids an integer as well.

src/Lynx/Engine.cs
@@ -12,7 +12,7 @@ public sealed partial class Engine : IDisposable
     internal const int DefaultMaxDepth = 5;
 
     private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
-    private readonly string _id;
+    private readonly int _id;
     private readonly ChannelWriter<object> _engineWriter;
     private readonly TranspositionTable _tt;
     private SearchConstraints _searchConstraints;
@@ -39,10 +39,10 @@ public sealed partial class Engine : IDisposable
 
     private CancellationToken _searchCancellationToken = CancellationToken.None;
     private CancellationToken _absoluteSearchCancellationToken = CancellationToken.None;
-    public Engine(ChannelWriter<object> engineWriter) : this("0", engineWriter, new()) { }
+    public Engine(ChannelWriter<object> engineWriter) : this(0, engineWriter, new()) { }
 
 #pragma warning disable RCS1163 // Unused parameter - used in Release mode
-    public Engine(string id, ChannelWriter<object> engineWriter, in TranspositionTable tt, bool warmup = false)
+    public Engine(int id, ChannelWriter<object> engineWriter, in TranspositionTable tt, bool warmup = false)
 #pragma warning restore RCS1163 // Unused parameter
     {
         _id = id;

src/Lynx/Model/SearchResult.cs
@@ -5,7 +5,7 @@ namespace Lynx.Model;
 public sealed class SearchResult
 {
 #if MULTITHREAD_DEBUG
-    public string EngineId { get; init; }
+    public int EngineId { get; init; }
 #endif
 
     public Move[] Moves { get; init; }
@@ -32,14 +32,14 @@ public sealed class SearchResult
 
 #if MULTITHREAD_DEBUG
     public SearchResult(Move bestMove, int score, int targetDepth, Move[] moves, int mate = default)
-        : this("-1", bestMove, score, targetDepth, moves, mate)
+        : this(-2, bestMove, score, targetDepth, moves, mate)
     {
     }
 #endif
 
     public SearchResult(
 #if MULTITHREAD_DEBUG
-        string engineId,
+        int engineId,
 #endif
         Move bestMove, int score, int targetDepth, Move[] moves, int mate = default)
     {

src/Lynx/Searcher.cs
@@ -12,7 +12,7 @@ public sealed class Searcher
     private readonly ChannelWriter<object> _engineWriter;
     private readonly Logger _logger;
 
-    internal const string MainEngineId = "1";
+    internal const int MainEngineId = 1;
 
     private int _searchThreadsCount;
     private Engine _mainEngine;
@@ -277,7 +277,7 @@ public sealed class Searcher
 
     public async ValueTask RunBench(int depth)
     {
-        using var engine = new Engine("bench", SilentChannelWriter<object>.Instance, in _ttWrapper, warmup: true);
+        using var engine = new Engine(-1, SilentChannelWriter<object>.Instance, in _ttWrapper, warmup: true);
         var results = engine.Bench(depth);
 
         // Can't use engine, or results won't be printed
@@ -286,7 +286,7 @@ public sealed class Searcher
 
     public async ValueTask RunVerboseBench(int depth)
     {
-        using var engine = new Engine("verbosebench", _engineWriter, in _ttWrapper, warmup: true);
+        using var engine = new Engine(-1, _engineWriter, in _ttWrapper, warmup: true);
         var results = engine.Bench(depth);
 
         await engine.PrintBenchResults(results);
@@ -308,7 +308,7 @@ public sealed class Searcher
 
             for (int i = 0; i < _searchThreadsCount - mainEngineOffset; ++i)
             {
-                _extraEngines[i] = new Engine($"{i + 2}",
+                _extraEngines[i] = new Engine(i + 2,
 #if MULTITHREAD_DEBUG
                     _engineWriter,
 #else