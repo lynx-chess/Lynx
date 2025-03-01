d56729d068a9d01f2f6bab0d914377303faf579e
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-03-01 02:08:35 +0100

    Offset 6

---
+++ src/Lynx/Cuckoo.cs
@@ -0,0 +1,8 @@
+﻿namespace Lynx;
+
+public static class Cuckoo
+{
+    public static ulong Hash1(ulong key) => ((key >> 32) & 0x1fff);
+
+    public static ulong Hash2(ulong key) => ((key >> 48) & 0x1fff);
+}
