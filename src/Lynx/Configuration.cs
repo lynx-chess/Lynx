bce26239894aa959a547e6c7f83d1bc7b1219379
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-03-04 17:41:06 +0100

    Adapt values

--- src/Lynx/Configuration.cs
+++ src/Lynx/Configuration.cs
@@ -216,10 +216,10 @@ public sealed class EngineSettings
     public int LMR_History_Divisor_Noisy { get; set; } = 3200;
 
     [SPSA<int>(20, 100, 5)]
-    public int LMR_DeeperBase { get; set; } = 38;
+    public int LMR_DeeperBase { get; set; } = 50;
 
     //[SPSA<int>(1, 10, 1)]
-    public int LMR_DeeperDepthMultiplier { get; set; } = 2;
+    public int LMR_DeeperDepthMultiplier { get; set; } = 8;
 
     //[SPSA<int>(1, 10, 0.5)]
     public int NMP_MinDepth { get; set; } = 3;
