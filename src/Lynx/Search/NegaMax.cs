2109a79366f65dd3ff1ade8127b5bc81bdcaee8f
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-03-06 14:53:49 +0100

    Use the TT move condition only for RFP

--- src/Lynx/Search/NegaMax.cs
+++ src/Lynx/Search/NegaMax.cs
@@ -165,7 +165,7 @@ public sealed partial class Engine
 
                     var rfpThreshold = rfpMargin + improvingFactor;
 
-                    if (staticEval - rfpThreshold >= beta)
+                    if (staticEval - rfpThreshold >= beta && ttBestMove == default)
                     {
 #pragma warning disable S3949 // Calculations should not overflow - value is being set at the beginning of the else if (!pvNode)
                         return (staticEval + beta) / 2;
