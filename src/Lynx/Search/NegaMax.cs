59560f47ef49fb7b6bbbed88ee692073838e9f4a
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-03-31 00:09:54 +0200

    Stop using followup history for pruning

--- src/Lynx/Search/NegaMax.cs
+++ src/Lynx/Search/NegaMax.cs
@@ -294,8 +294,7 @@ public sealed partial class Engine
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             int QuietHistory() => quietHistory ??=
                 _quietHistory[move.Piece()][move.TargetSquare()]
-                + CounterMoveHistoryEntry(move.Piece(), move.TargetSquare(), ply)
-                + FollowUpHistoryEntry(move.Piece(), move.TargetSquare(), ply);
+                + CounterMoveHistoryEntry(move.Piece(), move.TargetSquare(), ply);
 
             // If we prune while getting checmated, we risk not finding any move and having an empty PV
             bool isNotGettingCheckmated = bestScore > EvaluationConstants.NegativeCheckmateDetectionLimit;
