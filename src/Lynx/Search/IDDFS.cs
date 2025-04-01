8aeff546547b529903089f0d9189cf184efbc09b
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-04-01 20:59:26 +0200

    Fix build

--- src/Lynx/Search/IDDFS.cs
+++ src/Lynx/Search/IDDFS.cs
@@ -500,7 +500,7 @@ public sealed partial class Engine
         ShortMove ttBestMove = default;
 
         using var position = new Position(Game.PositionBeforeLastSearch);
-        var ttEntry = _tt.ProbeHash(position, ply: 0, Game.HalfMovesWithoutCaptureOrPawnMove));
+        var ttEntry = _tt.ProbeHash(position, ply: 0, Game.HalfMovesWithoutCaptureOrPawnMove);
 
         if (ttEntry.NodeType != NodeType.Unknown)
         {
