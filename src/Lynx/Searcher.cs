a58f548c09e6a2dd5f14e9001f593b30cba36dc9
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-04-09 02:11:27 +0200

    Mimic thread voting from `mt/thread-voting-3`

--- src/Lynx/Searcher.cs
+++ src/Lynx/Searcher.cs
@@ -372,14 +372,65 @@ public sealed class Searcher
                     _logger.Debug("End of extra searches, {0} ms", sw.ElapsedMilliseconds - lastElapsed);
 #endif
 
+        var totalNodes = finalSearchResult?.Nodes ?? 0;
+        var totalTime = finalSearchResult?.Time ?? 0;
+
+        await foreach (var task in Task.WhenEach(tasks))
+        {
+            var extraResult = await task;
+
+            if (extraResult is not null)
+            {
+                totalNodes += extraResult.Nodes;
+                totalTime += extraResult.Time;
+
+                if (finalSearchResult is null)
+                {
+                    finalSearchResult = extraResult;
+                    continue;
+                }
+
+                // Thread voting, original impl sligtly corrected based on by Heimdall's (based on Berserk's)
+                if (extraResult.BestMove != default)
+                {
+                    finalSearchResult = finalSearchResult.Mate switch
+                    {
+                        0                                                                       // No mate detected in main thread:
+                            when                                                                //      Extra thread:
+                                extraResult.Mate > 0                                            //          Mate
+                                    || extraResult.Depth > finalSearchResult.Depth              //          ||  Higher depth
+                                    || (extraResult.Depth == finalSearchResult.Depth            //          ||  Same depth, better score
+                                        && extraResult.Score > finalSearchResult.Score)
+
+                                => extraResult,
+                        > 0                                                                     // Mating in main thread:
+                            when                                                                //      Extra thread:
+                                extraResult.Mate > 0                                            //          Still mating
+                                    && (extraResult.Mate < finalSearchResult.Mate               //          &&  [But faster (shorter mate)
+                                        || (extraResult.Mate == finalSearchResult.Mate          //              || Same mating distance, but higher depth]
+                                            && extraResult.Depth > finalSearchResult.Depth))
+
+                                => extraResult,
+
+                        < 0                                                                     // Mated in main thread:
+                            when                                                                //      Extra thread:
+                                extraResult.Depth > finalSearchResult.Depth                     //          Higher depth
+                                    || (extraResult.Depth == finalSearchResult.Depth            //          || Same depth
+                                        && (extraResult.Mate >= 0                               //              && [But mating or at least not mated any more
+                                            || extraResult.Mate < finalSearchResult.Mate))      //                  || Still mated, but longer mate]
+
+                                => extraResult,
+
+                        _ => finalSearchResult
+                    };
+                }
+            }
+        }
+
         if (finalSearchResult is not null)
         {
-            // We wait just for the node count, so there's room for improvement here with thread voting
-            // and other strategies that take other thread results into account
-            await foreach (var extraResult in Task.WhenEach(tasks))
-            {
-                finalSearchResult.Nodes += (await extraResult)?.Nodes ?? 0;
-            }
+            finalSearchResult.Nodes = totalNodes;
+            finalSearchResult.Time = totalTime;
 
             finalSearchResult.NodesPerSecond = Utils.CalculateNps(finalSearchResult.Nodes, 0.001 * finalSearchResult.Time);
 
