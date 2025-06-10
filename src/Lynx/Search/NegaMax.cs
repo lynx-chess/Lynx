3789cca4d64998feae9bbec22efaf5d4a703abd3
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-06-11 00:25:57 +0200

    Add double negative extension

--- src/Lynx/Search/NegaMax.cs
+++ src/Lynx/Search/NegaMax.cs
@@ -439,6 +439,12 @@ public sealed partial class Engine
                 else if (ttScore >= beta)
                 {
                     --singularDepthExtensions;
+
+                    // Double negative extension
+                    if (ttScore + Configuration.EngineSettings.SE_DoubleNegativeExtensons_Margin >= beta)
+                    {
+                        --singularDepthExtensions;
+                    }
                 }
 
                 gameState = position.MakeMove(move);
