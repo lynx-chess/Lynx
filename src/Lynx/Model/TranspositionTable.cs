27b1ffe85f125c3f6fcd24380afdbe11cc828c3d
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2025-08-24 19:17:49 +0200

    🐛 TT static eval saving: depth overflow  (#2011)

--- src/Lynx/Model/TranspositionTable.cs
+++ src/Lynx/Model/TranspositionTable.cs
@@ -163,7 +163,7 @@ public readonly struct TranspositionTable
         ref var entry = ref _tt[ttIndex];
 
         // Extra key checks here (right before saving) failed for MT in https://github.com/lynx-chess/Lynx/pull/1566
-        entry.Update(position.UniqueIdentifier, EvaluationConstants.NoScore, staticEval, depth: -1, NodeType.None, wasPv ? 1 : 0, null);
+        entry.Update(position.UniqueIdentifier, EvaluationConstants.NoScore, staticEval, depth: 0, NodeType.None, wasPv ? 1 : 0, null);
     }
 
     /// <summary>
