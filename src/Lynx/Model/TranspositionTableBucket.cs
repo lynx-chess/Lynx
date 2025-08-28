964daaccc46956b921b62eb20621d356567e2378
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-08-28 02:30:47 +0200

    Add padding

--- src/Lynx/Model/TranspositionTableBucket.cs
+++ src/Lynx/Model/TranspositionTableBucket.cs
@@ -6,8 +6,8 @@ namespace Lynx.Model;
 /// <summary>
 /// A pointer to this (TranspositionTableBucket*) can be casted to a TranspositionTableElement* and indexed from 0 to <see cref="Constants.TranspositionTableElementsPerBucket"/> - 1 to access each entry.
 /// </summary>
-[StructLayout(LayoutKind.Explicit, Size = 30)]
-public struct TranspositionTableBucket
+[StructLayout(LayoutKind.Explicit, Size = 32)]
+public unsafe struct TranspositionTableBucket
 {
 #pragma warning disable S1144, RCS1213 // Unused private types or members should be removed
     [FieldOffset(0)]
@@ -18,9 +18,10 @@ public struct TranspositionTableBucket
 
     [FieldOffset(20)]
     private TranspositionTableElement _ttEntry2;
-#pragma warning restore S1144 // Unused private types or members should be removed
 
-    // TODO Add byte padding to align with 32 or 64 bytes
+    [FieldOffset(30)]
+    private fixed byte _padding[2];
+#pragma warning restore S1144 // Unused private types or members should be removed
 
     /// <summary>
     /// Struct size in bytes
