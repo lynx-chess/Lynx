4693745dfd8b61ccac98c358c3b3f0d2748d6859
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-09-07 03:24:58 +0200

    Missing one

--- tests/Lynx.Test/Model/MoveToEPDStringTest.cs
+++ tests/Lynx.Test/Model/MoveToEPDStringTest.cs
@@ -97,6 +97,7 @@ public class MoveToEPDStringTest
         Span<BitBoard> attacks = stackalloc BitBoard[12];
         Span<BitBoard> attacksBySide = stackalloc BitBoard[2];
         var evaluationContext = new EvaluationContext(attacks, attacksBySide);
+        evaluationContext.EnsureThreatsAreCalculated(position);
 
         var pseudoLegalMoves = MoveGenerator.GenerateAllMoves(position, ref evaluationContext, moves).ToArray();
 
