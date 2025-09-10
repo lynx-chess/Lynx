1c47692a199933ef7c7982c7e2d98399789ef30b
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-09-10 16:15:05 +0200

    Readd GC.SuppressFinalize(this);

--- src/Lynx/Model/Game.cs
+++ src/Lynx/Model/Game.cs
@@ -284,5 +284,9 @@ public sealed class Game : IDisposable
     {
         // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
         Dispose(disposing: true);
+
+#pragma warning disable S3234 // "GC.SuppressFinalize" should not be invoked for types without destructors - https://learn.microsoft.com/en-us/dotnet/standard/garbage-collection/implementing-dispose
+        GC.SuppressFinalize(this);
+#pragma warning restore S3234 // "GC.SuppressFinalize" should not be invoked for types without destructors
     }
 }
