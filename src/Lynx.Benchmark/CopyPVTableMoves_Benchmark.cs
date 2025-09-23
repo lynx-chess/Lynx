452b39cf6850f087e285f663bccc1f8671308348
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-09-23 12:55:51 +0200

    Add benchmark results

--- src/Lynx.Benchmark/CopyPVTableMoves_Benchmark.cs
+++ src/Lynx.Benchmark/CopyPVTableMoves_Benchmark.cs
@@ -1,5 +1,44 @@
+/*
+ *  BenchmarkDotNet v0.15.3, Linux Ubuntu 24.04.3 LTS (Noble Numbat)
+ *  AMD EPYC 7763 2.45GHz, 1 CPU, 4 logical and 2 physical cores
+ *  .NET SDK 9.0.305
+ *    [Host]     : .NET 9.0.9 (9.0.9, 9.0.925.41916), X64 RyuJIT x86-64-v3
+ *    DefaultJob : .NET 9.0.9 (9.0.9, 9.0.925.41916), X64 RyuJIT x86-64-v3
+ *
+ *  | Method                          | plyDepth | Mean       | Error    | StdDev  | Ratio | RatioSD | Allocated | Alloc Ratio |
+ *  |-------------------------------- |--------- |-----------:|---------:|--------:|------:|--------:|----------:|------------:|
+ *  | CopyPVTableMoves_Original       | 1        |   429.2 ns |  2.00 ns | 1.77 ns |  1.00 |    0.01 |         - |          NA |
+ *  | CopyPVTableMoves_Pointers       | 1        | 4,442.1 ns |  4.29 ns | 3.35 ns | 10.35 |    0.04 |         - |          NA |
+ *  | CopyPVTableMoves_Clear_Original | 1        |   428.0 ns |  0.21 ns | 0.18 ns |  1.00 |    0.00 |         - |          NA |
+ *  | CopyPVTableMoves_Clear_Pointers | 1        | 5,725.8 ns | 10.58 ns | 8.83 ns | 13.34 |    0.06 |         - |          NA |
+ *  |                                 |          |            |          |         |       |         |           |             |
+ *  | CopyPVTableMoves_Original       | 5        |   400.7 ns |  0.22 ns | 0.21 ns |  1.00 |    0.00 |         - |          NA |
+ *  | CopyPVTableMoves_Pointers       | 5        | 4,213.9 ns |  4.06 ns | 3.39 ns | 10.52 |    0.01 |         - |          NA |
+ *  | CopyPVTableMoves_Clear_Original | 5        |   400.1 ns |  0.14 ns | 0.11 ns |  1.00 |    0.00 |         - |          NA |
+ *  | CopyPVTableMoves_Clear_Pointers | 5        | 5,409.1 ns |  8.39 ns | 7.01 ns | 13.50 |    0.02 |         - |          NA |
+ *  |                                 |          |            |          |         |       |         |           |             |
+ *  | CopyPVTableMoves_Original       | 10       |   372.8 ns |  0.20 ns | 0.17 ns |  1.00 |    0.00 |         - |          NA |
+ *  | CopyPVTableMoves_Pointers       | 10       | 3,912.9 ns |  1.98 ns | 1.65 ns | 10.50 |    0.01 |         - |          NA |
+ *  | CopyPVTableMoves_Clear_Original | 10       |   372.0 ns |  0.29 ns | 0.26 ns |  1.00 |    0.00 |         - |          NA |
+ *  | CopyPVTableMoves_Clear_Pointers | 10       | 5,024.0 ns |  4.64 ns | 3.62 ns | 13.48 |    0.01 |         - |          NA |
+ *  |                                 |          |            |          |         |       |         |           |             |
+ *  | CopyPVTableMoves_Original       | 20       |   319.4 ns |  0.17 ns | 0.14 ns |  1.00 |    0.00 |         - |          NA |
+ *  | CopyPVTableMoves_Pointers       | 20       | 3,353.0 ns |  3.01 ns | 2.51 ns | 10.50 |    0.01 |         - |          NA |
+ *  | CopyPVTableMoves_Clear_Original | 20       |   322.3 ns |  0.28 ns | 0.26 ns |  1.01 |    0.00 |         - |          NA |
+ *  | CopyPVTableMoves_Clear_Pointers | 20       | 4,309.3 ns |  3.24 ns | 2.71 ns | 13.49 |    0.01 |         - |          NA |
+ *  |                                 |          |            |          |         |       |         |           |             |
+ *  | CopyPVTableMoves_Original       | 30       |   269.0 ns |  0.17 ns | 0.14 ns |  1.00 |    0.00 |         - |          NA |
+ *  | CopyPVTableMoves_Pointers       | 30       | 2,850.0 ns |  2.34 ns | 2.08 ns | 10.59 |    0.01 |         - |          NA |
+ *  | CopyPVTableMoves_Clear_Original | 30       |   273.5 ns |  0.25 ns | 0.21 ns |  1.02 |    0.00 |         - |          NA |
+ *  | CopyPVTableMoves_Clear_Pointers | 30       | 3,667.9 ns |  5.31 ns | 4.96 ns | 13.64 |    0.02 |         - |          NA |
+ *  |                                 |          |            |          |         |       |         |           |             |
+ *  | CopyPVTableMoves_Original       | 40       |   229.5 ns |  0.14 ns | 0.11 ns |  1.00 |    0.00 |         - |          NA |
+ *  | CopyPVTableMoves_Pointers       | 40       | 2,398.2 ns |  0.88 ns | 0.69 ns | 10.45 |    0.01 |         - |          NA |
+ *  | CopyPVTableMoves_Clear_Original | 40       |   231.1 ns |  0.39 ns | 0.36 ns |  1.01 |    0.00 |         - |          NA |
+ *  | CopyPVTableMoves_Clear_Pointers | 40       | 3,094.2 ns |  5.44 ns | 5.09 ns | 13.48 |    0.02 |         - |          NA |
+ */
+
 using BenchmarkDotNet.Attributes;
-using EnvDTE;
 using Lynx.Model;
 using System.Runtime.CompilerServices;
 
