611236642dc79cb70b28a14bba1a5115de8946c2
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-09-23 03:17:00 +0200

    Cleanup benchmark and add results

--- src/Lynx.Benchmark/CalculateThreats_Benchmark.cs
+++ src/Lynx.Benchmark/CalculateThreats_Benchmark.cs
@@ -1,8 +1,57 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿/*
+ *  BenchmarkDotNet v0.14.0, Ubuntu 24.04.3 LTS (Noble Numbat)
+ *  AMD EPYC 7763, 1 CPU, 4 logical and 2 physical cores
+ *  .NET SDK 9.0.305
+ *    [Host]     : .NET 9.0.9 (9.0.925.41916), X64 RyuJIT AVX2
+ *    DefaultJob : .NET 9.0.9 (9.0.925.41916), X64 RyuJIT AVX2
+ *
+ *  | Method    | Mean     | Error     | StdDev    | Ratio | Allocated | Alloc Ratio |
+ *  |---------- |---------:|----------:|----------:|------:|----------:|------------:|
+ *  | Original  | 8.832 us | 0.0445 us | 0.0416 us |  1.00 |      80 B |        1.00 |
+ *  | Reference | 8.034 us | 0.0358 us | 0.0317 us |  0.91 |      80 B |        1.00 |
+ *
+ *
+ *  BenchmarkDotNet v0.14.0, macOS Sequoia 15.6.1 (24G90) [Darwin 24.6.0]
+ *  Apple M1 (Virtual), 1 CPU, 3 logical and 3 physical cores
+ *  .NET SDK 9.0.305
+ *    [Host]     : .NET 9.0.9 (9.0.925.41916), Arm64 RyuJIT AdvSIMD
+ *    DefaultJob : .NET 9.0.9 (9.0.925.41916), Arm64 RyuJIT AdvSIMD
+ *
+ *  | Method    | Mean     | Error     | StdDev    | Median   | Ratio | RatioSD | Gen0   | Allocated | Alloc Ratio |
+ *  |---------- |---------:|----------:|----------:|---------:|------:|--------:|-------:|----------:|------------:|
+ *  | Original  | 7.661 us | 0.3157 us | 0.9058 us | 7.303 us |  1.01 |    0.16 |      - |      80 B |        1.00 |
+ *  | Reference | 7.457 us | 0.3365 us | 0.9817 us | 7.208 us |  0.99 |    0.17 | 0.0076 |      80 B |        1.00 |
+ *
+ *
+ *  BenchmarkDotNet v0.14.0, Windows 11 (10.0.26100.4946) (Hyper-V)
+ *  Unknown processor
+ *  .NET SDK 9.0.305
+ *    [Host]     : .NET 9.0.9 (9.0.925.41916), X64 RyuJIT AVX2
+ *    DefaultJob : .NET 9.0.9 (9.0.925.41916), X64 RyuJIT AVX2
+ *
+ *  | Method    | Mean     | Error     | StdDev    | Ratio | RatioSD | Allocated | Alloc Ratio |
+ *  |---------- |---------:|----------:|----------:|------:|--------:|----------:|------------:|
+ *  | Original  | 7.227 us | 0.0269 us | 0.0239 us |  1.00 |    0.00 |      80 B |        1.00 |
+ *  | Reference | 6.887 us | 0.1372 us | 0.2292 us |  0.95 |    0.03 |      80 B |        1.00 |
+ *
+ *
+ *  BenchmarkDotNet v0.14.0, macOS Ventura 13.7.6 (22H625) [Darwin 22.6.0]
+ *  Intel Core i7-8700B CPU 3.20GHz (Max: 3.19GHz) (Coffee Lake), 1 CPU, 4 logical and 4 physical cores
+ *  .NET SDK 9.0.305
+ *    [Host]     : .NET 9.0.9 (9.0.925.41916), X64 RyuJIT AVX2
+ *    DefaultJob : .NET 9.0.9 (9.0.925.41916), X64 RyuJIT AVX2
+ *
+ *  | Method    | Mean     | Error    | StdDev   | Median   | Ratio | RatioSD | Allocated | Alloc Ratio |
+ *  |---------- |---------:|---------:|---------:|---------:|------:|--------:|----------:|------------:|
+ *  | Original  | 19.30 us | 0.878 us | 2.520 us | 18.50 us |  1.02 |    0.18 |      80 B |        1.00 |
+ *  | Reference | 20.60 us | 0.946 us | 2.668 us | 20.72 us |  1.08 |    0.19 |      80 B |        1.00 |
+ */
+
+using BenchmarkDotNet.Attributes;
 using Lynx.Model;
-using System.Buffers;
 
 namespace Lynx.Benchmark;
+
 public class CalculateThreats_Benchmark : BaseBenchmark
 {
     private readonly Position_CalculateThreats_Benchmark[] _positions;
@@ -43,35 +92,10 @@ public class CalculateThreats_Benchmark : BaseBenchmark
 
 class Position_CalculateThreats_Benchmark
 {
-    internal int _incrementalEvalAccumulator;
-    internal int _incrementalPhaseAccumulator;
-    internal bool _isIncrementalEval;
-
     private readonly ulong[] _pieceBitBoards;
     private readonly ulong[] _occupancyBitBoards;
-    private readonly int[] _board;
 
-    private readonly byte _castle;
-
-#pragma warning disable S3887, CA1051
-    private readonly byte[] _castlingRightsUpdateConstants;
-    public readonly ulong[] KingsideCastlingFreeSquares;
-    public readonly ulong[] KingsideCastlingNonAttackedSquares;
-    public readonly ulong[] QueensideCastlingFreeSquares;
-    public readonly ulong[] QueensideCastlingNonAttackedSquares;
-
-#pragma warning disable IDE1006 // Naming Styles
-    internal readonly int WhiteShortCastle;
-    internal readonly int WhiteLongCastle;
-    internal readonly int BlackShortCastle;
-    internal readonly int BlackLongCastle;
-#pragma warning restore IDE1006 // Naming Styles
-#pragma warning restore S3887, CA1051
-
-    private readonly BoardSquare _enPassant;
-    private readonly Side _side;
-
-#pragma warning disable RCS1085 // Use auto-implemented property
+    #pragma warning disable RCS1085 // Use auto-implemented property
 
     /// <summary>
     /// Use <see cref="Piece"/> as index
@@ -83,30 +107,7 @@ class Position_CalculateThreats_Benchmark
     /// </summary>
     public BitBoard[] OccupancyBitBoards => _occupancyBitBoards;
 
-    /// <summary>
-    /// Piece location indexed by square
-    /// </summary>
-    public int[] Board => _board;
-
-    public Side Side => _side;
-
-    public BoardSquare EnPassant => _enPassant;
-
-    /// <summary>
-    /// See <see cref="<CastlingRights"/>
-    /// </summary>
-    public byte Castle { get => _castle; }
-
 #pragma warning restore RCS1085 // Use auto-implemented property
-
-    public BitBoard Queens => _pieceBitBoards[(int)Piece.Q] | _pieceBitBoards[(int)Piece.q];
-    public BitBoard Rooks => _pieceBitBoards[(int)Piece.R] | _pieceBitBoards[(int)Piece.r];
-    public BitBoard Bishops => _pieceBitBoards[(int)Piece.B] | _pieceBitBoards[(int)Piece.b];
-    public BitBoard Knights => _pieceBitBoards[(int)Piece.N] | _pieceBitBoards[(int)Piece.n];
-    public BitBoard Kings => _pieceBitBoards[(int)Piece.K] | _pieceBitBoards[(int)Piece.k];
-
-    public int WhiteKingSquare => _pieceBitBoards[(int)Piece.K].GetLS1BIndex();
-    public int BlackKingSquare => _pieceBitBoards[(int)Piece.k].GetLS1BIndex();
 
     /// <summary>
     /// Beware, half move counter isn't take into account
@@ -120,118 +121,6 @@ class Position_CalculateThreats_Benchmark
     {
         _pieceBitBoards = parsedFEN.PieceBitBoards;
         _occupancyBitBoards = parsedFEN.OccupancyBitBoards;
-        _board = parsedFEN.Board;
-
-        _side = parsedFEN.Side;
-        _castle = parsedFEN.Castle;
-        _enPassant = parsedFEN.EnPassant;
-
-        _isIncrementalEval = false;
-
-        _castlingRightsUpdateConstants = ArrayPool<byte>.Shared.Rent(64);
-        Array.Fill(_castlingRightsUpdateConstants, Constants.NoUpdateCastlingRight, 0, 64);
-
-        // It won't be possible to add castling rights to a position created froma FEN without them
-        if (_castle == (int)CastlingRights.None)
-        {
-            KingsideCastlingFreeSquares = [];
-            QueensideCastlingFreeSquares = [];
-            KingsideCastlingNonAttackedSquares = [];
-            QueensideCastlingNonAttackedSquares = [];
-        }
-        else
-        {
-            var whiteKingSquare = WhiteKingSquare;
-            var blackKingSquare = BlackKingSquare;
-
-            _castlingRightsUpdateConstants[whiteKingSquare] = Constants.WhiteKingCastlingRight;
-            _castlingRightsUpdateConstants[blackKingSquare] = Constants.BlackKingCastlingRight;
-
-            var castlingData = parsedFEN.CastlingData;
-
-            var whiteKingsideRook = castlingData.WhiteKingsideRook;
-            if (whiteKingsideRook != CastlingData.DefaultValues)
-            {
-                _castlingRightsUpdateConstants[whiteKingsideRook] = Constants.WhiteKingSideRookCastlingRight;
-            }
-
-            var whiteQueensideRook = castlingData.WhiteQueensideRook;
-            if (whiteQueensideRook != CastlingData.DefaultValues)
-            {
-                _castlingRightsUpdateConstants[whiteQueensideRook] = Constants.WhiteQueenSideRookCastlingRight;
-            }
-
-            var blackKingsideRook = castlingData.BlackKingsideRook;
-            if (blackKingsideRook != CastlingData.DefaultValues)
-            {
-                _castlingRightsUpdateConstants[blackKingsideRook] = Constants.BlackKingSideRookCastlingRight;
-            }
-
-            var blackQueensideRook = castlingData.BlackQueensideRook;
-            if (blackQueensideRook != CastlingData.DefaultValues)
-            {
-                _castlingRightsUpdateConstants[blackQueensideRook] = Constants.BlackQueenSideRookCastlingRight;
-            }
-
-            KingsideCastlingNonAttackedSquares = ArrayPool<ulong>.Shared.Rent(2);
-            KingsideCastlingNonAttackedSquares[(int)Side.White] = BitBoardExtensions.MaskBetweenTwoSquaresSameRankInclusive(whiteKingSquare, Constants.WhiteKingShortCastleSquare);
-            KingsideCastlingNonAttackedSquares[(int)Side.Black] = BitBoardExtensions.MaskBetweenTwoSquaresSameRankInclusive(blackKingSquare, Constants.BlackKingShortCastleSquare);
-
-            QueensideCastlingNonAttackedSquares = ArrayPool<ulong>.Shared.Rent(2);
-            QueensideCastlingNonAttackedSquares[(int)Side.White] = BitBoardExtensions.MaskBetweenTwoSquaresSameRankInclusive(whiteKingSquare, Constants.WhiteKingLongCastleSquare);
-            QueensideCastlingNonAttackedSquares[(int)Side.Black] = BitBoardExtensions.MaskBetweenTwoSquaresSameRankInclusive(blackKingSquare, Constants.BlackKingLongCastleSquare);
-
-            // This could be simplified/harcoded for standard chess, see FreeAndNonAttackedSquares
-            KingsideCastlingFreeSquares = ArrayPool<ulong>.Shared.Rent(2);
-
-            var whiteKingsideFreeMask = KingsideCastlingNonAttackedSquares[(int)Side.White]
-                | BitBoardExtensions.MaskBetweenTwoSquaresSameRankInclusive(whiteKingsideRook, Constants.WhiteRookShortCastleSquare);
-            whiteKingsideFreeMask.PopBit(whiteKingSquare);
-            whiteKingsideFreeMask.PopBit(whiteKingsideRook);
-
-            var blackKingsideFreeMask = KingsideCastlingNonAttackedSquares[(int)Side.Black]
-                | BitBoardExtensions.MaskBetweenTwoSquaresSameRankInclusive(blackKingsideRook, Constants.BlackRookShortCastleSquare);
-            blackKingsideFreeMask.PopBit(blackKingSquare);
-            blackKingsideFreeMask.PopBit(blackKingsideRook);
-
-            KingsideCastlingFreeSquares[(int)Side.White] = whiteKingsideFreeMask;
-            KingsideCastlingFreeSquares[(int)Side.Black] = blackKingsideFreeMask;
-
-            // This could be simplified/harcoded for standard chess, see FreeAndNonAttackedSquares
-            QueensideCastlingFreeSquares = ArrayPool<ulong>.Shared.Rent(2);
-
-            var whiteQueensideFreeMask = QueensideCastlingNonAttackedSquares[(int)Side.White]
-                | BitBoardExtensions.MaskBetweenTwoSquaresSameRankInclusive(whiteQueensideRook, Constants.WhiteRookLongCastleSquare);
-            whiteQueensideFreeMask.PopBit(whiteKingSquare);
-            whiteQueensideFreeMask.PopBit(whiteQueensideRook);
-
-            var blackQueensideFreeMask = QueensideCastlingNonAttackedSquares[(int)Side.Black]
-                | BitBoardExtensions.MaskBetweenTwoSquaresSameRankInclusive(blackQueensideRook, Constants.BlackRookLongCastleSquare);
-            blackQueensideFreeMask.PopBit(blackKingSquare);
-            blackQueensideFreeMask.PopBit(blackQueensideRook);
-
-            QueensideCastlingFreeSquares[(int)Side.White] = whiteQueensideFreeMask;
-            QueensideCastlingFreeSquares[(int)Side.Black] = blackQueensideFreeMask;
-
-            // Usual encoding for standard chess, King to target square
-            if (!Configuration.EngineSettings.IsChess960)
-            {
-                WhiteShortCastle = MoveExtensions.EncodeShortCastle(whiteKingSquare, Constants.WhiteKingShortCastleSquare, (int)Piece.K);
-                WhiteLongCastle = MoveExtensions.EncodeLongCastle(whiteKingSquare, Constants.WhiteKingLongCastleSquare, (int)Piece.K);
-
-                BlackShortCastle = MoveExtensions.EncodeShortCastle(blackKingSquare, Constants.BlackKingShortCastleSquare, (int)Piece.k);
-                BlackLongCastle = MoveExtensions.EncodeLongCastle(blackKingSquare, Constants.BlackKingLongCastleSquare, (int)Piece.k);
-            }
-            // KxR encoding for DFRC
-            else
-            {
-                WhiteShortCastle = MoveExtensions.EncodeShortCastle(whiteKingSquare, whiteKingsideRook, (int)Piece.K);
-                WhiteLongCastle = MoveExtensions.EncodeLongCastle(whiteKingSquare, whiteQueensideRook, (int)Piece.K);
-
-                BlackShortCastle = MoveExtensions.EncodeShortCastle(blackKingSquare, blackKingsideRook, (int)Piece.k);
-                BlackLongCastle = MoveExtensions.EncodeLongCastle(blackKingSquare, blackQueensideRook, (int)Piece.k);
-            }
-        }
     }
 
     public EvaluationContext CalculateThreats_Original(ref EvaluationContext evaluationContext)
