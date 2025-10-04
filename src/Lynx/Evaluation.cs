6f92356ba3d4431c0fabb4a4db53405346283407
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-10-04 13:25:22 +0200

    Leave the early eg detection at the beginning in the tuning, but not in the evalation code

--- src/Lynx/Evaluation.cs
+++ src/Lynx/Evaluation.cs
@@ -67,117 +67,13 @@ public partial class Position
         //    return result;
         //}
 
-        const int initialEndgameScaling = 128;
-        int endgameScaling = initialEndgameScaling;
-        int gamePhase = Phase();
-
         var whitePawns = _pieceBitBoards[(int)Piece.P];
         var blackPawns = _pieceBitBoards[(int)Piece.p];
 
         var totalPawnsCount = (whitePawns | blackPawns).CountBits();
 
-        // Few pieces endgames
-        if (gamePhase <= 5)
-        {
-            // Pawnless endgames
-            if (totalPawnsCount == 0)
-            {
-                switch (gamePhase)
-                {
-                    case 5:
-                        {
-                            // RB vs R, RN vs R - scale it down due to the chances of it being a draw
-                            if (_pieceBitBoards[(int)Piece.R].CountBits() == 1 && _pieceBitBoards[(int)Piece.r].CountBits() == 1)
-                            {
-                                endgameScaling >>= 1; // /2
-                            }
-
-                            break;
-                        }
-                    case 4:
-                        {
-                            // Rook vs 2 minors and R vs r should be a draw
-                            if ((_pieceBitBoards[(int)Piece.R] != 0 && (_pieceBitBoards[(int)Piece.B] | _pieceBitBoards[(int)Piece.N]) == 0)
-                                || (_pieceBitBoards[(int)Piece.r] != 0 && (_pieceBitBoards[(int)Piece.b] | _pieceBitBoards[(int)Piece.n]) == 0))
-                            {
-                                endgameScaling >>= 1; // /2
-                            }
-
-                            break;
-                        }
-                    case 3:
-                        {
-                            var sideWithMorePieces = Utils.PieceOffset(_occupancyBitBoards[(int)Side.White].CountBits() >= _occupancyBitBoards[(int)Side.Black].CountBits());
-
-                            if (_pieceBitBoards[(int)Piece.N + sideWithMorePieces].CountBits() == 2)      // NN vs N, NN vs B
-                            {
-                                return (0, gamePhase);
-                            }
-
-                            // Rook vs a minor is a draw
-                            // Without rooks, only BB vs N is a win and BN vs N can have some chances
-
-                            endgameScaling >>= 1; // /2
-
-                            break;
-                        }
-                    case 2:
-                        {
-                            var whiteKnightsCount = _pieceBitBoards[(int)Piece.N].CountBits();
-
-                            if (whiteKnightsCount + _pieceBitBoards[(int)Piece.n].CountBits() == 2            // NN vs -, N vs N
-                                    || whiteKnightsCount + _pieceBitBoards[(int)Piece.B].CountBits() == 1)    // B vs N, B vs B
-                            {
-                                return (0, gamePhase);
-                            }
-
-                            break;
-                        }
-                    case 1:
-                    case 0:
-                        {
-                            return (0, gamePhase);
-                        }
-                }
-            }
-            else
-            {
-                var sideWithMorePieces = Utils.PieceOffset(_occupancyBitBoards[(int)Side.White].CountBits() >= _occupancyBitBoards[(int)Side.Black].CountBits());
-
-                if (gamePhase == 1)
-                {
-                    if (_pieceBitBoards[(int)Piece.B + sideWithMorePieces] != 0
-                        && (_pieceBitBoards[(int)Piece.P + sideWithMorePieces] & Constants.NotAorH) == 0)
-                    {
-                        //if (IsBishopPawnDraw(winningSideOffset))
-                        //{
-                        //    return (0, gamePhase);
-                        //}
-
-                        // We can reduce the rest of positions, i.e. if the king hasn't reached the corner
-                        // This also reduces won positions, but it shouldn't matter
-                        endgameScaling >>= 1; // /2
-                    }
-                }
-                else if (gamePhase == 2)
-                {
-                    var whiteBishops = _pieceBitBoards[(int)Piece.B];
-                    var blackBishops = _pieceBitBoards[(int)Piece.b];
-
-                    // Opposite color bishop endgame with pawns are drawish
-                    if (whiteBishops > 0
-                        && blackBishops > 0
-                        && Constants.DarkSquares[whiteBishops.GetLS1BIndex()] !=
-                            Constants.DarkSquares[blackBishops.GetLS1BIndex()])
-                    {
-                        endgameScaling >>= 1; // /2
-                    }
-                }
-            }
-        }
-
         int packedScore = 0;
-        gamePhase = 0;
+        int gamePhase = 0;
 
         BitBoard whitePawnAttacks = whitePawns.ShiftUpRightAndLeft();
         BitBoard blackPawnAttacks = blackPawns.ShiftDownRightAndLeft();
@@ -493,8 +389,105 @@ public partial class Position
         var endGameScore = Utils.UnpackEG(packedScore);
         var eval = ((middleGameScore * gamePhase) + (endGameScore * endGamePhase)) / MaxPhase;
 
-        eval *= endgameScaling;
-        eval /= initialEndgameScaling;
+        // Few pieces endgames
+        if (gamePhase <= 5)
+        {
+            // Pawnless endgames
+            if (totalPawnsCount == 0)
+            {
+                switch (gamePhase)
+                {
+                    case 5:
+                        {
+                            // RB vs R, RN vs R - scale it down due to the chances of it being a draw
+                            if (_pieceBitBoards[(int)Piece.R].CountBits() == 1 && _pieceBitBoards[(int)Piece.r].CountBits() == 1)
+                            {
+                                eval >>= 1; // /2
+                            }
+
+                            break;
+                        }
+                    case 4:
+                        {
+                            // Rook vs 2 minors and R vs r should be a draw
+                            if ((_pieceBitBoards[(int)Piece.R] != 0 && (_pieceBitBoards[(int)Piece.B] | _pieceBitBoards[(int)Piece.N]) == 0)
+                                || (_pieceBitBoards[(int)Piece.r] != 0 && (_pieceBitBoards[(int)Piece.b] | _pieceBitBoards[(int)Piece.n]) == 0))
+                            {
+                                eval >>= 1; // /2
+                            }
+
+                            break;
+                        }
+                    case 3:
+                        {
+                            var sideWithMorePieces = Utils.PieceOffset(_occupancyBitBoards[(int)Side.White].CountBits() >= _occupancyBitBoards[(int)Side.Black].CountBits());
+
+                            if (_pieceBitBoards[(int)Piece.N + sideWithMorePieces].CountBits() == 2)      // NN vs N, NN vs B
+                            {
+                                return (0, gamePhase);
+                            }
+
+                            // Rook vs a minor is a draw
+                            // Without rooks, only BB vs N is a win and BN vs N can have some chances
+
+                            eval >>= 1; // /2
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            var whiteKnightsCount = _pieceBitBoards[(int)Piece.N].CountBits();
+
+                            if (whiteKnightsCount + _pieceBitBoards[(int)Piece.n].CountBits() == 2            // NN vs -, N vs N
+                                    || whiteKnightsCount + _pieceBitBoards[(int)Piece.B].CountBits() == 1)    // B vs N, B vs B
+                            {
+                                return (0, gamePhase);
+                            }
+
+                            break;
+                        }
+                    case 1:
+                    case 0:
+                        {
+                            return (0, gamePhase);
+                        }
+                }
+            }
+            else
+            {
+                var sideWithMorePieces = Utils.PieceOffset(_occupancyBitBoards[(int)Side.White].CountBits() >= _occupancyBitBoards[(int)Side.Black].CountBits());
+
+                if (gamePhase == 1)
+                {
+                    if (_pieceBitBoards[(int)Piece.B + sideWithMorePieces] != 0
+                        && (_pieceBitBoards[(int)Piece.P + sideWithMorePieces] & Constants.NotAorH) == 0)
+                    {
+                        //if (IsBishopPawnDraw(winningSideOffset))
+                        //{
+                        //    return (0, gamePhase);
+                        //}
+
+                        // We can reduce the rest of positions, i.e. if the king hasn't reached the corner
+                        // This also reduces won positions, but it shouldn't matter
+                        eval >>= 1; // /2
+                    }
+                }
+                else if (gamePhase == 2)
+                {
+                    var whiteBishops = _pieceBitBoards[(int)Piece.B];
+                    var blackBishops = _pieceBitBoards[(int)Piece.b];
+
+                    // Opposite color bishop endgame with pawns are drawish
+                    if (whiteBishops > 0
+                        && blackBishops > 0
+                        && Constants.DarkSquares[whiteBishops.GetLS1BIndex()] !=
+                            Constants.DarkSquares[blackBishops.GetLS1BIndex()])
+                    {
+                        eval >>= 1; // /2
+                    }
+                }
+            }
+        }
 
         // Endgame scaling with pawn count, formula yoinked from Sirius
         eval = (int)(eval * ((80 + (totalPawnsCount * 7)) / 128.0));
