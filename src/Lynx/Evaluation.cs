910662f2e38698dca941c319b93bb027eaaf7ef8
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2025-12-25 13:55:06 +0100

    ⚖️ Passed pawn push bonus - split by rank (#2318)

--- src/Lynx/Evaluation.cs
+++ src/Lynx/Evaluation.cs
@@ -1044,10 +1044,24 @@ public partial class Position
         var thirdRank = Masks.RankMasks[side == Side.White ? (int)BoardSquare.a3 : (int)BoardSquare.a6];
         var doublePushes = ~occupancy & (pushes & thirdRank).PawnPush(side);
         pushes |= doublePushes;
+        var safePushes = pushes & safe;
 
-        var pushThreats = (pushes & safe).PawnAttacks(side) & nonPawnEnemies;
+        var pushThreats = safePushes.PawnAttacks(side) & nonPawnEnemies;
 
         packedBonus += PawnPushThreatBonus * pushThreats.CountBits();
+
+        while (safePushes != 0)
+        {
+            safePushes = safePushes.WithoutLS1B(out var safePush);
+            var isWhite = side == Side.White;
+            var passedPawnMask = isWhite ? Masks.WhitePassedPawnMasks[safePush] : Masks.BlackPassedPawnMasks[safePush];
+
+            if ((passedPawnMask & theirPawns) == 0)
+            {
+                var rank = isWhite ? Constants.Rank[safePush] : 7 - Constants.Rank[safePush];
+                packedBonus += PassedPawnPushBonus[rank];
+            }
+        }
 
         return packedBonus;
     }
