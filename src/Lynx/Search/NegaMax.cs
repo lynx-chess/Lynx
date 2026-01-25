c3d8e991219ebcb2771c91da9813d24b908ce821
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2026-01-26 00:15:13 +0100

    Use `newDepth` for deeper/shallower

--- src/Lynx/Search/NegaMax.cs
+++ src/Lynx/Search/NegaMax.cs
@@ -646,10 +646,10 @@ public sealed partial class Engine
                         // It should produce more cutoffs and therefore be faster.
                         // https://web.archive.org/web/20071030220825/http://www.brucemo.com/compchess/programming/pvs.htm
 
-                        var deeper = score > bestScore + Configuration.EngineSettings.LMR_DeeperBase + (Configuration.EngineSettings.LMR_DeeperDepthMultiplier * depth);
-                        var shallower = score < bestScore + depth;
+                        var deeper = score > bestScore + Configuration.EngineSettings.LMR_DeeperBase + (Configuration.EngineSettings.LMR_DeeperDepthMultiplier * newDepth);
+                        var shallower = score < bestScore + newDepth;
 
-                        if (deeper && !shallower && depth < Configuration.EngineSettings.MaxDepth)
+                        if (deeper && !shallower && newDepth < Configuration.EngineSettings.MaxDepth)
                         {
                             ++newDepth;
                         }
