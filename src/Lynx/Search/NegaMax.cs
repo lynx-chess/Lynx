b3e68f275ebe3c6e66afb5dde35f143358d33df9
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2026-01-27 19:31:27 +0100

    Factor ttPv in SE min depth (seen in Hobbes)

--- src/Lynx/Search/NegaMax.cs
+++ src/Lynx/Search/NegaMax.cs
@@ -452,7 +452,7 @@ public sealed partial class Engine
             if (
                 //!isVerifyingSE        // Implicit, otherwise the move would have been skipped already
                 isBestMove      // Ensures !isRoot and TT hit (otherwise there wouldn't be a TT move)
-                && depth >= Configuration.EngineSettings.SE_MinDepth
+                && depth >= Configuration.EngineSettings.SE_MinDepth + (ttPv ? 1 : 0)
                 && ttEntry.Depth + Configuration.EngineSettings.SE_TTDepthOffset >= depth
                 && Math.Abs(ttEntry.Score) < EvaluationConstants.PositiveCheckmateDetectionLimit
                 && ttEntry.NodeType != NodeType.Alpha
