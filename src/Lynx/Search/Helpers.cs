eba213b5f6004926aea4da9ecdcf462f5825b376
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2026-02-12 12:14:16 +0100

    Add butterfly quiet history and average quiet history value with the existing piece-to

--- src/Lynx/Search/Helpers.cs
+++ src/Lynx/Search/Helpers.cs
@@ -49,7 +49,7 @@ public sealed partial class Engine
     /// [12][64][2][2]
     /// </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private ref short QuietHistoryEntry(Position position, Move move, ref EvaluationContext evaluationContext)
+    private ref short PieceToQuietHistoryEntry(Position position, Move move, ref EvaluationContext evaluationContext)
     {
         const int pieceOffset = 64 * 2 * 2;
         const int targetSquareOffset = 2 * 2;
@@ -69,7 +69,43 @@ public sealed partial class Engine
             + (isStartSquareAttacked * startSquareAttackedOffset)
             + isTargetSquareAttacked;
 
-        return ref _quietHistory[index];
+        return ref _pieceToQuietHistory[index];
+    }
+
+    /// <summary>
+    /// [64][64][2][2]
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private ref short ButterflyQuietHistoryEntry(Position position, Move move, ref EvaluationContext evaluationContext)
+    {
+        const int sourceSquareOffset = 64 * 2 * 2;
+        const int targetSquareOffset = 2 * 2;
+        const int startSquareAttackedOffset = 2;
+
+        var sourceSquare = move.SourceSquare();
+        var targetSquare = move.TargetSquare();
+        var oppositeSide = Utils.OppositeSide((int)position.Side);
+
+        var oppositeSideAttacks = evaluationContext.AttacksBySide[oppositeSide];
+
+        var isStartSquareAttacked = oppositeSideAttacks.GetBit(sourceSquare) ? 1 : 0;
+        var isTargetSquareAttacked = oppositeSideAttacks.GetBit(targetSquare) ? 1 : 0;
+
+        var index = (sourceSquare * sourceSquareOffset)
+            + (targetSquare * targetSquareOffset)
+            + (isStartSquareAttacked * startSquareAttackedOffset)
+            + isTargetSquareAttacked;
+
+        return ref _butterflyQuietHistory[index];
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private short QuietHistoryEntry(Position position, Move move, ref EvaluationContext evaluationContext)
+    {
+        var pieceToHistory = PieceToQuietHistoryEntry(position, move, ref evaluationContext);
+        var butterflyHistory = ButterflyQuietHistoryEntry(position, move, ref evaluationContext);
+
+        return (short)((pieceToHistory + butterflyHistory) / 2);
     }
 
     /// <summary>
@@ -294,12 +330,34 @@ public sealed partial class Engine
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private unsafe void AgeQuietHistory()
     {
-        // Since _quietHistory is a pinned array
-        // This is no-op pinning as it does not influence the GC compaction
+        // Since history arrays are pinned arrays
+        // These are no-op pinning as it does not influence the GC compaction
         // https://tooslowexception.com/pinned-object-heap-in-net-5/
-        fixed (short* histPtr = _quietHistory)
+        fixed (short* histPtr = _pieceToQuietHistory)
         {
-            for (int i = 0; i < QuietHistoryLength; i += 4)
+            for (int i = 0; i < PieceToQuietHistoryLength; i += 4)
+            {
+                short* start = histPtr + i;
+
+                short* h2 = start + 1;
+                short* h3 = start + 2;
+                short* h4 = start + 3;
+
+                int tmp1 = *start * 3;
+                int tmp2 = *h2 * 3;
+                int tmp3 = *h3 * 3;
+                int tmp4 = *h4 * 3;
+
+                *start = (short)(tmp1 / 4);
+                *h2 = (short)(tmp2 / 4);
+                *h3 = (short)(tmp3 / 4);
+                *h4 = (short)(tmp4 / 4);
+            }
+        }
+
+        fixed (short* histPtr = _butterflyQuietHistory)
+        {
+            for (int i = 0; i < ButterflyHistoryLength; i += 4)
             {
                 short* start = histPtr + i;
 
