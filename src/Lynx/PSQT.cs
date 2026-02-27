d6a81051d1c55e0eafe067320e3f305bde67a2a5
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2026-02-27 15:15:27 +0100

    Different bucket layout

--- src/Lynx/PSQT.cs
+++ src/Lynx/PSQT.cs
@@ -27,12 +27,12 @@ public static class EvaluationPSQTs
     public static ReadOnlySpan<int> PawnBucketLayout =>
     [
         0, 0, 0, 0, 0, 0, 0, 0,
-        5, 5, 6, 6, 7, 7, 8, 8,
-        5, 5, 6, 6, 7, 7, 8, 8,
-        4, 4, 5, 5, 5, 5, 6, 6,
-        4, 4, 5, 5, 5, 5, 6, 6,
-        1, 1, 2, 2, 2, 2, 3, 3,
-        1, 1, 2, 2, 2, 2, 3, 3,
+        9, 9, 10, 10, 10, 10, 9, 9,
+        7, 7, 8, 8, 8, 8, 7, 7,
+        5, 5, 6, 6, 6, 6, 5, 5,
+        3, 3, 4, 4, 4, 4, 3, 3,
+        1, 1, 2, 2, 2, 2, 1, 1,
+        1, 1, 2, 2, 2, 2, 1, 1,
         0, 0, 0, 0, 0, 0, 0, 0, //
     ];
 #pragma warning restore S4663 // Comments should not be empty
