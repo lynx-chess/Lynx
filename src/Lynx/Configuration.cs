3d72697b6d4c71e5960f492ae668f02691beff34
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2026-04-06 13:29:38 +0200

    Probcut_BetaMargin 200 -> 300

--- src/Lynx/Configuration.cs
+++ src/Lynx/Configuration.cs
@@ -497,8 +497,8 @@ public sealed class EngineSettings
     [SPSA<int>(enabled: false)]
     public int Probcut_MinDepth { get; set; } = 7;
 
-    [SPSA<int>(100, 300, 20)]
-    public int Probcut_BetaMargin { get; set; } = 200;
+    [SPSA<int>(100, 500, 50)]
+    public int Probcut_BetaMargin { get; set; } = 300;
 
     [SPSA<int>(1, 5, 1)]
     public int Probcut_DepthOffset { get; set; } = 3;
