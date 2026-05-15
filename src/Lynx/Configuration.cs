49f6d16e25d5027d9d4333446c7d9489f4370673
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2026-05-15 13:03:56 +0200

    Reduce weight to 50

--- src/Lynx/Configuration.cs
+++ src/Lynx/Configuration.cs
@@ -477,7 +477,7 @@ public sealed class EngineSettings
     /// Needs to be re-scaled dividing by <see cref="EvaluationConstants.CorrHistScaleFactor"/>
     /// </summary>
     [SPSA<int>(50, 250, 15)]
-    public int CorrHistoryWeight_Continuation { get; set; } = 100;
+    public int CorrHistoryWeight_Continuation { get; set; } = 50;
 
     [SPSA<int>(enabled: false)]
     public int TT_50MR_Start { get; set; } = 20;
