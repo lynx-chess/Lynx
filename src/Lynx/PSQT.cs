156c2fc7d902f2718911a3525cee851b8dc2ebd3
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2026-08-25 03:41:37 +0200

    Isolate kingside corner ```diff      15, 16, 17, 18, 19, 20, 21, 22,      15, 8, 9, 10, 11, 12, 13, 14,      15, 8, 9, 10, 11, 12, 13, 14, -    0, 8, 9, 10, 11, 12, 13, 14, +    15, 8, 9, 10, 11, 12, 13, 14,      0, 1, 2, 3, 4, 5, 6, 7, //  }; ```

--- src/Lynx/PSQT.cs
+++ src/Lynx/PSQT.cs
@@ -20,7 +20,7 @@ public static class EvaluationPSQTs
         15, 16, 17, 18, 19, 20, 21, 22,
         15, 8, 9, 10, 11, 12, 13, 14,
         15, 8, 9, 10, 11, 12, 13, 14,
-        0, 8, 9, 10, 11, 12, 13, 14,
+        15, 8, 9, 10, 11, 12, 13, 14,
         0, 1, 2, 3, 4, 5, 6, 7, //
     ];
 #pragma warning restore S4663 // Comments should not be empty
