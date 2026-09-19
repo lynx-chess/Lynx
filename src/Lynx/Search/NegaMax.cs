5800f68880da032cfe21f927eae05005eb6ba2ce
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2026-09-19 13:47:57 +0200

    Alt

--- src/Lynx/Search/NegaMax.cs
+++ src/Lynx/Search/NegaMax.cs
@@ -652,39 +652,39 @@ public sealed partial class Engine
                     {
                         // Search with reduced depth and zero window
                         score = -NegaMax(reducedDepth, ply + 1, -alpha - 1, -alpha, cutnode: true, cancellationToken);
-                    }
 
-                    // 🔍 Principal Variation Search (PVS)
-                    if (score > alpha && newDepth > reducedDepth)
-                    {
-                        // Optimistic search, validating that the rest of the moves are worse than bestmove.
-                        // It should produce more cutoffs and therefore be faster.
-                        // https://web.archive.org/web/20071030220825/http://www.brucemo.com/compchess/programming/pvs.htm
-
-                        var deeper = score > bestScore + Configuration.EngineSettings.LMR_DeeperBase + (Configuration.EngineSettings.LMR_DeeperDepthMultiplier * depth);
-                        var shallower = score < bestScore + depth;
-
-                        if (deeper && !shallower && newDepth < Configuration.EngineSettings.MaxDepth)
+                        // 🔍 Principal Variation Search (PVS)
+                        if (score > alpha && newDepth > reducedDepth)
                         {
-                            ++newDepth;
-                        }
-                        else if (shallower && !deeper && newDepth > 1)
-                        {
-                            --newDepth;
-                        }
+                            // Optimistic search, validating that the rest of the moves are worse than bestmove.
+                            // It should produce more cutoffs and therefore be faster.
+                            // https://web.archive.org/web/20071030220825/http://www.brucemo.com/compchess/programming/pvs.htm
 
-                        if (newDepth > reducedDepth)
-                        {
-                            // Search with full depth but narrowed score bandwidth (zero-window search)
-                            score = -NegaMax(newDepth, ply + 1, -alpha - 1, -alpha, !cutnode, cancellationToken);
+                            var deeper = score > bestScore + Configuration.EngineSettings.LMR_DeeperBase + (Configuration.EngineSettings.LMR_DeeperDepthMultiplier * depth);
+                            var shallower = score < bestScore + depth;
+
+                            if (deeper && !shallower && newDepth < Configuration.EngineSettings.MaxDepth)
+                            {
+                                ++newDepth;
+                            }
+                            else if (shallower && !deeper && newDepth > 1)
+                            {
+                                --newDepth;
+                            }
+
+                            if (newDepth > reducedDepth)
+                            {
+                                // Search with full depth but narrowed score bandwidth (zero-window search)
+                                score = -NegaMax(newDepth, ply + 1, -alpha - 1, -alpha, !cutnode, cancellationToken);
+                            }
+
+                            // 🔍 Post-LMR continuation history update
+                            var historyBonus = score > alpha
+                                ? EvaluationConstants.HistoryBonus[depth]
+                                : -EvaluationConstants.HistoryMalus[depth];
+
+                            UpdateContinuationHistory(piece, targetSquare, ply, historyBonus);
                         }
-
-                        // 🔍 Post-LMR continuation history update
-                        var historyBonus = score > alpha
-                            ? EvaluationConstants.HistoryBonus[depth]
-                            : -EvaluationConstants.HistoryMalus[depth];
-
-                        UpdateContinuationHistory(piece, targetSquare, ply, historyBonus);
                     }
                 }
 
