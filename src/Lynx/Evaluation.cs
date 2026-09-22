2e1d5c60f27fec4763b32af675a9b2e067201c68
Eduardo Cáceres <11148519+eduherminio@users.noreply.github.com>
2026-09-23 01:23:40 +0200

    ⚡ Minor optimization when accessing `GamePhaseByPiece` (#2685)

--- src/Lynx/Evaluation.cs
+++ src/Lynx/Evaluation.cs
@@ -222,6 +222,8 @@ public partial class Position
                 packedScore += pawnScore;
             }
 
+            ref var phaseByPiece = ref MemoryMarshal.GetReference(GamePhaseByPiece);
+
             // White pieces PSQTs and additional eval and pawn attacks, except king and pawn
             for (int pieceIndex = (int)Piece.N; pieceIndex < (int)Piece.K; ++pieceIndex)
             {
@@ -236,7 +238,7 @@ public partial class Position
 
                     IncrementalEvalAccumulator += PSQT(whiteBucket, blackBucket, pieceIndex, pieceSquareIndex);
 
-                    IncrementalPhaseAccumulator += GamePhaseByPiece[pieceIndex];
+                    IncrementalPhaseAccumulator += Unsafe.Add(ref phaseByPiece, pieceIndex);
 
                     packedScore += AdditionalPieceEvaluation(ref evaluationContext, pieceSquareIndex, whiteBucket, blackBucket, pieceIndex, (int)Side.White, blackPawnAttacks, blackKing);
                 }
@@ -257,7 +259,7 @@ public partial class Position
 
                     IncrementalEvalAccumulator += PSQT(blackBucket, whiteBucket, pieceIndex, pieceSquareIndex);
 
-                    IncrementalPhaseAccumulator += GamePhaseByPiece[pieceIndex];
+                    IncrementalPhaseAccumulator += Unsafe.Add(ref phaseByPiece, pieceIndex);
 
                     packedScore -= AdditionalPieceEvaluation(ref evaluationContext, pieceSquareIndex, blackBucket, whiteBucket, pieceIndex, (int)Side.Black, whitePawnAttacks, whiteKing);
                 }
@@ -297,7 +299,7 @@ public partial class Position
             .CountBits();
 
         packedScore += KingMobilityBonus[whiteKingAttacksCount]
-        - KingMobilityBonus[blackKingAttacksCount];
+            - KingMobilityBonus[blackKingAttacksCount];
 
         AssertAttackPopulation(ref evaluationContext);
 
@@ -479,10 +481,12 @@ public partial class Position
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal int PhaseFromScratch()
     {
-        return (Knights.CountBits() * GamePhaseByPiece[(int)Piece.N])
-            + (Bishops.CountBits() * GamePhaseByPiece[(int)Piece.B])
-            + (Rooks.CountBits() * GamePhaseByPiece[(int)Piece.R])
-            + (Queens.CountBits() * GamePhaseByPiece[(int)Piece.Q]);
+        ref var phaseByPiece = ref MemoryMarshal.GetReference(GamePhaseByPiece);
+
+        return (Knights.CountBits() * Unsafe.Add(ref phaseByPiece, (int)Piece.N))
+            + (Bishops.CountBits() * Unsafe.Add(ref phaseByPiece, (int)Piece.B))
+            + (Rooks.CountBits() * Unsafe.Add(ref phaseByPiece, (int)Piece.R))
+            + (Queens.CountBits() * Unsafe.Add(ref phaseByPiece, (int)Piece.Q));
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
